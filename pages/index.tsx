3e21515c21123da4eecb7704b0a5893d0c179519
Gabrio Frantzen <gabrio@jupiter.no>
2021-05-12 14:08:17 +0200

    Add layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import matter from "gray-matter";
 import { GetStaticProps } from "next";
-import renderToString from "next-mdx-remote/render-to-string";
-import { MdxRemote } from "next-mdx-remote/types";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 import { join } from "path";
 import styles from "../styles/Home.module.css";
 import fs from "fs";
+import Layout from "../components/layout";
 
 interface MarkdownFile {
   title: string;
@@ -22,72 +22,45 @@ interface Props {
 
 export default function Home({ latestNews }: Props) {
   return (
-    <div className={styles.grid_container}>
-      <div className={styles.header}>
-        <div className={styles.header__logo}>
-          <Link href="/">
-            <a rel="home">
-              <Image
-                src="/img/logos/SKDE_sort.png"
-                alt="Hjem"
-                width={96}
-                height={39}
-              />
-            </a>
-          </Link>
-        </div>
-        <div className={styles.header__links}>
-          <Link href="#">
-            <a>Kontakt</a>
-          </Link>
-          <div className={styles.fsz_helper}>
-            Tekststørrelse A+
-            <div className={styles.fsz_helper__container}>
-              ENDRE SKRIFTSTØRRELSE
-              <br />
-              Hold Ctrl-tasten nede.
-              <br />
-              Trykk på + for å forstørre eller - for å forminske, eventuelt
-              scroll med hjulet på musen.
-            </div>
+    <Layout>
+      <div className={`${styles.banner_section} ${styles.fullBleed}`}>
+        <div className={styles.content_container}>
+          <div className={styles.banner_section_article}>
+            <h2>Kvalitet og forbruk i helsetjenesten</h2>
+            <p>
+              Senter for klinisk dokumentasjon og evaluering (SKDE) jobber med å
+              kartlegge og synliggjøre geografiske ulikheter i
+              spesialisthelsetjenester. Målet er å bidra til likeverdige
+              helsetjenester av god kvalitet uansett hvor pasientene bor.
+            </p>
+            <Link href="#">
+              <a>Les mer om SKDE</a>
+            </Link>
           </div>
-        </div>
-      </div>
-
-      <div className={styles.banner_section}>
-        <div className={styles.banner_section_article}>
-          <h2>Kvalitet og forbruk i helsetjenesten</h2>
-          <p>
-            Senter for klinisk dokumentasjon og evaluering (SKDE) jobber med å
-            kartlegge og synliggjøre geografiske ulikheter i
-            spesialisthelsetjenester. Målet er å bidra til likeverdige
-            helsetjenester av god kvalitet uansett hvor pasientene bor.
-          </p>
-          <Link href="#">
-            <a>Les mer om SKDE</a>
-          </Link>
         </div>
       </div>
 
       <div className={styles.buttons}>
-        <h2>Resultater</h2>
-        <div className={styles.block_buttons}>
-          <div className={styles.block_button}>
-            <Link href="https://www.kvalitetsregistre.no/">
-              <a>Kvalitetsregistre</a>
-            </Link>
-          </div>
+        <div className={styles.content_container}>
+          <h2>Resultater</h2>
+          <div className={styles.block_buttons}>
+            <div className={styles.block_button}>
+              <Link href="https://www.kvalitetsregistre.no/">
+                <a>Kvalitetsregistre</a>
+              </Link>
+            </div>
 
-          <div className={styles.block_button}>
-            <Link href="https://helseatlas.no/">
-              <a>Helseatlas</a>
-            </Link>
-          </div>
+            <div className={styles.block_button}>
+              <Link href="https://helseatlas.no/">
+                <a>Helseatlas</a>
+              </Link>
+            </div>
 
-          <div className={styles.block_button}>
-            <Link href="https://helse-nord.no/skde/pasientstrommer">
-              <a>Pasientstrømmer</a>
-            </Link>
+            <div className={styles.block_button}>
+              <Link href="https://helse-nord.no/skde/pasientstrommer">
+                <a>Pasientstrømmer</a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -109,7 +82,7 @@ export default function Home({ latestNews }: Props) {
           <h2>Aktuelt</h2>
         </div>
         <div className={styles.news_section_title__link}>
-          <Link href="#">
+          <Link href="/news">
             <a>Se flere nyheter fra skde</a>
           </Link>
         </div>
@@ -124,60 +97,15 @@ export default function Home({ latestNews }: Props) {
           </Link>
         </div>
         <div className={styles.news_article__image}>
-          <Image src={`/${latestNews.thumbnail}`} height="300" width="500" />
-        </div>
-      </div>
-
-      <div className={styles.footer}>
-        <div className={styles.footer__logo}>
-          <Image src="/img/logos/SKDE_hvit_lys.png" height="40" width="99" />
-        </div>
-        <div className={styles.footer__contact}>
-          <p>
-            <strong>Telefon: </strong>
-            <a href="tel:77 75 58 00">77 75 58 00</a>
-            <br />
-            <strong>E-post:</strong>{" "}
-            <Link href="mailto:servicemiljoet@skde.no">
-              <a>servicemiljoet@skde.no</a>
-            </Link>
-          </p>
-        </div>
-        <div className={styles.footer__social}>
-          <a href="https://www.facebook.com/medisinskekvalitetsregistre">
-            Følg oss på Facebook
-          </a>
-          <Link href="https://helse-nord.no/skde/kontakt-skde">
-            <a>Kontakt</a>
-          </Link>
-          <Link href="https://helse-nord.no/om-nettstedet/personvern">
-            <a>Personvern</a>
-          </Link>
-        </div>
-
-        <div className={styles.footer__partner_l}>
           <Image
-            src="/img/logos/hf_nord-white.svg"
-            height="40px"
-            width="180px"
-          />
-        </div>
-        <div className={styles.footer__partner_m}>
-          <Image
-            src="/img/logos/NSM_logo_hvit.png"
-            height="40px"
-            width="359px"
-          />
-        </div>
-        <div className={styles.footer_partner_r}>
-          <Image
-            src="/img/logos/Logo_atlas_hvit.png"
-            height="40px"
-            width="146px"
+            src={`/${latestNews.thumbnail}`}
+            height="300"
+            width="500"
+            alt={latestNews.title}
           />
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
@@ -190,8 +118,10 @@ export const getStaticProps: GetStaticProps = async () => {
     return { content, data, slug: slug.replace(/.md?$/, "") };
   });
 
-  newsArticles.sort((a, b) => b.data.date - a.data.date);
-  const latestNewsArticle = newsArticles[0];
+  const latestNewsArticle = newsArticles.sort(
+    (a, b) => b.data.date - a.data.date
+  )[0];
+
   return {
     props: {
       latestNews: {
