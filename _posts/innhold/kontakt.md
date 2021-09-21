3956616de6bb963b9c8c9b2b165b88108f6950cc
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2021-09-21 17:34:21 +0200

    Markdown files of content (#76)
    Every markdown page in folder `_posts/innhold` will be a page directly under skde.no.
    For instance, the file `kontakt.md` will be the page skde.no/kontakt.
    It will also be possible to modify and make new pages through netlify CMS.

---
+++ _posts/innhold/kontakt.md
@@ -0,0 +1,27 @@
+---
+title: Kontakt SKDE
+ingress: Senter for klinisk dokumentasjon og evaluering (SKDE) holder i 3.etasje i SIVA-bygget i Forskningsparken i Tromsø.
+---
+**Telefon**  
+77 75 58 00
+
+**E-post**  
+postmottak@helse-nord.no
+
+**Postadresse**  
+Helse Nord RHF, SKDE  
+Postboks 1445  
+8083 Bodø
+
+**Besøksadresse**  
+SKDE Senter for klinisk evaluering og dokumentasjon  
+Helse Nord RHF  
+Sykehusveien 23, 3\. etasje  
+9019 Tromsø
+
+**Fakturaadresse**  
+Helse Nord RHF  
+Fakturamottak  
+Postboks 3230  
+7439 Trondheim eller elektronisk: invoice.3230@kollektor.no  
+Referanse som må angis ved fakturering er: 3200
---
+++ pages/[slug].tsx
@@ -0,0 +1,66 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+import renderToString from "next-mdx-remote/render-to-string";
+import hydrate from "next-mdx-remote/hydrate";
+import { MdxRemote } from "next-mdx-remote/types";
+import newsStyles from "../styles/News.module.css";
+import Layout from "../components/layout";
+const CONTENT_DIR = join(process.cwd(), "_posts/innhold");
+
+interface Props {
+  source: MdxRemote.Source;
+  frontMatter: any;
+}
+
+const Box = () => {
+  return (
+    <div style={{ backgroundColor: "blue", height: 500, width: 500 }}></div>
+  );
+};
+
+const Content = ({ source, frontMatter }: Props) => {
+  const content = hydrate(source, { components: { Box } });
+  return (
+    <Layout page={frontMatter.title}>
+      <div className={newsStyles.container}>
+        <div className={newsStyles.article__title}>
+          <h2>{frontMatter.title}</h2>
+        </div>
+        <div className={newsStyles.article}>
+          <div className={newsStyles.article__ingress}>
+            {frontMatter.ingress}
+          </div>
+          <div className={newsStyles.article__content}>{content}</div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Content;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const fullPath = join(CONTENT_DIR, `${params.slug}.md`);
+  const file = fs.readFileSync(fullPath);
+
+  const { content, data } = matter(file);
+  const source = await renderToString(content);
+  return {
+    props: {
+      source,
+      frontMatter: {
+        ...data
+      },
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = fs.readdirSync(CONTENT_DIR).map((file) => ({
+    params: { slug: file.replace(/.md?$/, "") },
+  }));
+
+  return { paths, fallback: false };
+};
