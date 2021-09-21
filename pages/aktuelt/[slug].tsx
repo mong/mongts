3284e874d92944d6bf6bdf8c513c746e867c6084
arnfinn <arnfinn.steindal@gmail.com>
2021-09-21 13:20:35 +0200

    Date on news without time

--- pages/aktuelt/[slug].tsx
+++ pages/aktuelt/[slug].tsx
@@ -56,7 +56,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { content, data } = matter(file);
   const source = await renderToString(content);
   return {
-    props: { source, frontMatter: { ...data, date: dateToString(data.date) } },
+    props: { source, frontMatter: { ...data, date: dateToString(data.date, false) } },
   };
 };
 
