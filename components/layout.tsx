c86cd2bb3dcdc25abe2d6d87cf60178193d9ae3f
yte0 <Yohannes.Tesfay@helse-nord.no>
2021-11-04 10:08:00 +0100

    debugging

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,20 +14,7 @@ export default function Layout({
   title = "SKDE - Helse Nord RHF",
   page,
 }: Props) {
-  useEffect(() => {
-    (function (h, o, t, j, a, r) {
-      /* @ts-ignore */
-      h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-      /* @ts-ignore */
-      h._hjSettings = { hjid: 2684483, hjsv: 6 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script'); r.async = 1;
-      /* @ts-ignore */
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 
-  }, [])
   return (
     <>
       <Head>
