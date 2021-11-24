93df7548f4c0ba4129e84c7f0c430b231687aa52
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2021-11-24 10:33:34 +0100

    Remove news from front page (#122)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
-import matter from "gray-matter";
-import { GetStaticProps } from "next";
 import Link from "next/link";
-import { join } from "path";
 import styles from "../styles/Home.module.css";
-import { dateToString } from "../helpers/dateHelpers";
 
-import fs from "fs";
 import Layout from "../components/layout";
 
 interface MarkdownFile {
@@ -20,7 +15,7 @@ interface Props {
   latestNews: MarkdownFile;
 }
 
-export default function Home({ latestNews }: Props) {
+export default function Home() {
   return (
     <Layout>
       <div className={styles.full_bleed}>
@@ -66,60 +61,6 @@ export default function Home({ latestNews }: Props) {
           </div>
         </div>
       </div>
-
-      <div className={`${styles.full_bleed} ${styles.news_section__container}`}>
-        <div className={styles.news_section}>
-          <div className={styles.news_section_title}>
-            <h2>Aktuelt</h2>
-          </div>
-          <div className={styles.news_section_title__link}>
-            <Link href="/aktuelt">
-              <a>Se flere nyheter fra skde</a>
-            </Link>
-          </div>
-          <div className={styles.news_section_article}>
-            <h3>{latestNews.title}</h3>
-            <small>{latestNews.date}</small>
-            <p>{latestNews.ingress}</p>
-          </div>
-          <div className={styles.news_section_article__image}>
-            <img
-              src={`/${latestNews.thumbnail}`}
-              width="100%"
-              alt={latestNews.title}
-            />
-          </div>
-          <div className={styles.news_section_article__button}>
-            <Link href={`/aktuelt/${latestNews.slug}`}>
-              <a>Les mer</a>
-            </Link>
-          </div>
-        </div>
-      </div>
     </Layout>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const NEWS_DIR = join(process.cwd(), "_posts/aktuelt");
-  const newsArticles = fs.readdirSync(NEWS_DIR).map((slug) => {
-    const fullPath = join(NEWS_DIR, slug);
-    const file = fs.readFileSync(fullPath);
-    const { content, data } = matter(file);
-    return { content, data, slug: slug.replace(/.md?$/, "") };
-  });
-
-  const latestNewsArticle = newsArticles.sort(
-    (a, b) => b.data.date - a.data.date
-  )[0];
-
-  return {
-    props: {
-      latestNews: {
-        ...latestNewsArticle.data,
-        date: dateToString(latestNewsArticle.data.date, false),
-        slug: latestNewsArticle.slug,
-      },
-    },
-  };
-};
