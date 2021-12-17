3e9ad76d64e0f7f35691f6c5fa4bdee82ed9d74a
arnfinn <arnfinn.steindal@gmail.com>
2021-12-17 14:20:43 +0100

    one more try

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,14 +1,15 @@
-describe('Navigation', () => {
-    it('should navigate to the about page', () => {
-      // Start from the index page
-      cy.visit('http://localhost:3000/')
-  
-      // Find a link with an href attribute containing "about" and click it
-      cy.get('a[href*="kontakt"]').click({ multiple: true })
-  
-      // The new url should include "/about"
-      cy.url().should('include', '/kontakt')
-  
-      cy.get('h2').contains('Kontakt SKDE')
-    })
-  })
+describe("Navigation", () => {
+  it("should navigate to the contact page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
+
+    // Find multiple link with an href attribute containing "kontakt" and click them
+    cy.get('a[href*="kontakt"]').click({ multiple: true });
+
+    // The new url should include "/kontakt"
+    cy.url().should("include", "/kontakt");
+
+    // The page should include the 'h2' tag "Kontakt SKDE"
+    cy.get("h2").contains("Kontakt SKDE");
+  });
+});
