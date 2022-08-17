7ed9f2add06577870525d6404d36f68c714770c0
arnfinn <arnfinn.steindal@gmail.com>
2022-08-17 20:44:33 +0200

    cleanup

--- pages/admin/cms.tsx
+++ pages/admin/cms.tsx
@@ -3,24 +3,10 @@ import CMS from "netlify-cms-app";
 import React from "react";
 import { config } from "./config";
 
-const Preview = (props) => {
-  const entry = props.entry;
-  const title = entry.getIn(["data", "title"]) || "";
-
-  return (
-    <>
-      <h1>{props.widgetFor("title")}</h1>
-      {props.widgetFor("body")}
-    </>
-  );
-};
-
 const Admin = () => {
   useEffect(() => {
     CMS.init({ config });
-    CMS.registerPreviewTemplate("blog", Preview);
   }, []);
-
   return <></>;
 };
 
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 
 const Admin = dynamic(() => import("./cms"), {
   ssr: false,
+  loading: () => <p>Loading Admin...</p>,
 });
 
 export default Admin;
