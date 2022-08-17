98c76dedc5483f5f6bda7cebdf67b7138846b930
arnfinn <arnfinn.steindal@gmail.com>
2022-08-17 10:28:14 +0200

    try to route to index.html helseatlas page
    Working on #305

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,13 +40,13 @@ export default function Home() {
           <h2>Resultater</h2>
           <div className={styles.block_buttons}>
             <div className={styles.block_button}>
-              <Link href="/kvalitetsregistre/">
+              <Link href="/kvalitetsregistre/alle/sykehus">
                 <a>Kvalitetsregistre</a>
               </Link>
             </div>
 
             <div className={styles.block_button}>
-              <Link href="/helseatlas/">
+              <Link href="/helseatlas/index.html">
                 <a>Helseatlas</a>
               </Link>
             </div>
