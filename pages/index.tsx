a9f60cf83412307251187284f1b67aba200c26be
arnfinn <arnfinn.steindal@gmail.com>
2022-08-23 14:38:50 +0200

    react-query

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import styles from "../src/styles/Home.module.css";
 import Layout from "../src/components/layout";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 if (process.env.NEXT_PUBLIC_SENTRY) {
   try {
@@ -26,46 +30,48 @@ export default function Home() {
     setOrigin(window.location.origin);
   }, [setOrigin]);
   return (
-    <Layout>
-      <div className={styles.full_bleed}>
-        <div className={styles.banner_article}>
-          <div className={styles.banner_article__content}>
-            <h2>Kvalitet og forbruk i helsetjenesten</h2>
-            <p>
-              Senter for klinisk dokumentasjon og evaluering (SKDE) jobber med å
-              kartlegge og synliggjøre geografiske ulikheter i
-              spesialisthelsetjenester. Målet er å bidra til likeverdige
-              helsetjenester av god kvalitet uansett hvor pasientene bor.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className={`${styles.full_bleed} ${styles.buttons_container}`}>
-        <div className={`${styles.buttons}`}>
-          <h2>Resultater</h2>
-          <div className={styles.block_buttons}>
-            <div className={styles.block_button}>
-              <Link href="/kvalitetsregistre">
-                <a>Kvalitetsregistre</a>
-              </Link>
-            </div>
-
-            <div className={styles.block_button}>
-              <a href={`${origin}/helseatlas/`}>Helseatlas</a>
-            </div>
-
-            <div className={styles.block_button}>
-              <Link href="/pasientstrommer">
-                <a>Pasientstrømmer</a>
-              </Link>
-            </div>
-
-            <div className={styles.block_button}>
-              <a href="https://helse-nord.no/skde">Om SKDE</a>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <div className={styles.full_bleed}>
+          <div className={styles.banner_article}>
+            <div className={styles.banner_article__content}>
+              <h2>Kvalitet og forbruk i helsetjenesten</h2>
+              <p>
+                Senter for klinisk dokumentasjon og evaluering (SKDE) jobber med
+                å kartlegge og synliggjøre geografiske ulikheter i
+                spesialisthelsetjenester. Målet er å bidra til likeverdige
+                helsetjenester av god kvalitet uansett hvor pasientene bor.
+              </p>
             </div>
           </div>
         </div>
-      </div>
-    </Layout>
+        <div className={`${styles.full_bleed} ${styles.buttons_container}`}>
+          <div className={`${styles.buttons}`}>
+            <h2>Resultater</h2>
+            <div className={styles.block_buttons}>
+              <div className={styles.block_button}>
+                <Link href="/kvalitetsregistre">
+                  <a>Kvalitetsregistre</a>
+                </Link>
+              </div>
+
+              <div className={styles.block_button}>
+                <a href={`${origin}/helseatlas/`}>Helseatlas</a>
+              </div>
+
+              <div className={styles.block_button}>
+                <Link href="/pasientstrommer">
+                  <a>Pasientstrømmer</a>
+                </Link>
+              </div>
+
+              <div className={styles.block_button}>
+                <a href="https://helse-nord.no/skde">Om SKDE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </QueryClientProvider>
   );
 }
