c787d74c60a0a26fd2b36cc3742073b614d0d272
arnfinn <arnfinn.steindal@gmail.com>
2022-08-23 09:12:06 +0200

    two level slug

--- pages/kvalitetsregistre/[register]/[tab].tsx
+++ pages/kvalitetsregistre/[register]/[tab].tsx
@@ -17,17 +17,19 @@ function Page({ data }) {
   // This gets called on every request
   export async function getServerSideProps(context) {
 
-    console.log(context.query.slug)
-    const API_HOST =
-  process.env.REACT_APP_API_HOST ?? "http://localhost:4000";
+    console.log(context.query.register)
+    console.log(context.query.tab)
+    const API_HOST = process.env.REACT_APP_API_HOST ?? "http://localhost:4000";
+    const register = context.query.slug;
     // Fetch data from external API
-    const res = await fetch(`https://dc9yut6ftb9m1.cloudfront.net/info/names`)
+    const res = await fetch(`${API_HOST}/info/names`)
     const data_tmp = await res.json()
 
 
-        const data = data_tmp.map((register) => {
-            return(register.rname)
-        })
+    const data = data_tmp.map((register) => {
+      return(register.rname)
+    })
+    
     
 //    } else {
 //        const data = data_tmp.map((register) => {
