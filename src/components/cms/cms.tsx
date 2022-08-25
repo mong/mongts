3721757210d4c5fe29c46d12cdf4397d4a7517ab
arnfinn <arnfinn.steindal@gmail.com>
2022-08-25 16:19:51 +0200

    back to newest version. Using it anyway

--- src/components/cms/cms.tsx
+++ src/components/cms/cms.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import { config } from "./config";
 
 const Admin = () => {
-  console.log("netlify-cms-app version 2.14.48");
-
   useEffect(() => {
     import("netlify-cms-app").then(async (cms: any) => {
       cms.init({ config });
