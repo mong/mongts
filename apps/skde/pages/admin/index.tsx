5fdaba491fa10f2c5b372aa96dd328b3361f260e
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-10-04 15:23:12 +0200

    remove admin page (#381)

--- apps/skde/pages/admin/index.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const InitializeCMS = dynamic(() => import("../../src/components/cms/cms"), {
-  ssr: false,
-  loading: () => <p>Loading Admin...</p>,
-});
-
-export default InitializeCMS;
