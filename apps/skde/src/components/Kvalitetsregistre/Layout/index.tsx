a370469d1387fc505fdeb46da2fd3d7c27c81158
arnfinn <arnfinn.steindal@gmail.com>
2022-10-07 12:26:54 +0200

    Try to include css by .kvalit...

--- apps/skde/src/components/Kvalitetsregistre/Layout/index.tsx
+++ apps/skde/src/components/Kvalitetsregistre/Layout/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { LayoutHead } from "./LayoutHead";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import styles from "../../../styles/Kvalitetsregistre.module.css";
 
 interface Props {
   children: any;
@@ -14,11 +15,11 @@ export default function Layout({ children }: Props) {
   }, [setOrigin]);
 
   return (
-    <>
+    <div className={styles.kvalitetsregister}>
       <LayoutHead />
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   );
 }
