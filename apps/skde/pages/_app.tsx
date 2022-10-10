029bf5b8b038d3905914442593a1a42512e53647
arnfinn <arnfinn.steindal@gmail.com>
2022-10-10 10:39:40 +0200

    Add (unused) react-query to _app.tsx

--- apps/skde/pages/_app.tsx
+++ apps/skde/pages/_app.tsx
@@ -1,8 +1,22 @@
 import "../src/styles/globals.css";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryParamProvider } from "use-query-params";
+import { NextAdapter } from "next-query-params";
+import { ReactQueryDevtools } from "react-query/devtools";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryParamProvider adapter={NextAdapter}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </QueryParamProvider>
+  );
 }
 
 export default MyApp;
