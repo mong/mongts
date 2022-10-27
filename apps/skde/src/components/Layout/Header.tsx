57d29b8c584ef6dd7362f0b36687078dfbd99364
arnfinn <arnfinn.steindal@gmail.com>
2022-10-27 13:13:43 +0200

    legacy/image only when needed

--- apps/skde/src/components/Layout/Header.tsx
+++ apps/skde/src/components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 import { imgLoader } from "../../helpers/functions";
 import classNames from "./Header.module.css";
