2b9227a3d9c3392bb885a41853283d846c83cade
arnfinn <arnfinn.steindal@gmail.com>
2022-11-25 15:45:10 +0100

    any -> DataProps

--- apps/skde/src/components/Context/index.tsx
+++ apps/skde/src/components/Context/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { MapData } from "../../types";
+import { MapData, DataProps } from "../../types";
 
-export const DataContext: React.Context<{ atlasData: any; mapData: MapData }> =
-  React.createContext({ atlasData: {}, mapData: undefined });
+export const DataContext: React.Context<{
+  atlasData: DataProps;
+  mapData: MapData;
+}> = React.createContext({
+  atlasData: { innhold: undefined },
+  mapData: undefined,
+});
