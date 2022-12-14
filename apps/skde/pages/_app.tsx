df0f97bd04a5ce4a24fc9986215459c2de84f341
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2022-12-14 11:11:48 +0100

    Bruk @next/font (#620)
    * Use @next/font
    
    Will automatically optimize font and remove external network requests.
    Closes #608
    
    * remove hard coded fonts in css
    
    * rm fontFamily

--- apps/skde/pages/_app.tsx
+++ apps/skde/pages/_app.tsx
@@ -5,17 +5,24 @@ import { QueryParamProvider } from "use-query-params";
 import { NextAdapter } from "next-query-params";
 import { ReactQueryDevtools } from "react-query/devtools";
 import type { AppProps } from "next/app";
+import { Open_Sans } from "@next/font/google";
+
+const open_sans = Open_Sans({
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryParamProvider adapter={NextAdapter}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </QueryParamProvider>
+    <main className={open_sans.className}>
+      <QueryParamProvider adapter={NextAdapter}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </QueryParamProvider>
+    </main>
   );
 }
 
