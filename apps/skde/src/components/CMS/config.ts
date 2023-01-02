9b6dd94db1eb99ff1a75c4b6385815f526348f43
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-01-02 09:34:58 +0100

    CMS-fix: ingress as markdown (#678)

--- apps/skde/src/components/CMS/config.ts
+++ apps/skde/src/components/CMS/config.ts
@@ -65,7 +65,7 @@ const atlas = (lang: "no" | "en"): CmsCollection => {
       {
         label: "Ingress",
         name: "ingress",
-        widget: "string",
+        widget: "markdown",
       },
       {
         label: "Språk",
