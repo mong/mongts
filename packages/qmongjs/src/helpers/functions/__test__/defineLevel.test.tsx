02bbd9e5a03c2eda4fc8018027dcdd4d7b91f67e
arnfinn <arnfinn.steindal@gmail.com>
2023-01-14 21:30:32 +0100

    expand test

---
+++ packages/qmongjs/src/helpers/functions/__test__/defineLevel.test.tsx
@@ -0,0 +1,52 @@
+import { level } from "../.";
+import { Indicator } from "types";
+
+const indicator: Indicator = {
+  id: 1,
+  ind_id: "a",
+  unit_level: "a",
+  unit_name: "a",
+  year: 2,
+  denominator: 2,
+  min_denominator: null,
+  var: 0.01,
+  level_direction: 0,
+  level_green: 0.05,
+  level_yellow: 0.1,
+  sformat: ",.0%",
+  dg: null,
+  include: 1,
+  type: "a",
+  delivery_time: new Date("October 13, 2014 11:13:00"),
+  delivery_latest_affirm: new Date("October 13, 2019 11:13:00"),
+};
+
+test("Returns correct level with direction 0", () => {
+  expect(level(indicator)).toBe("H");
+  indicator["var"] = 0.075;
+  expect(level(indicator)).toBe("M");
+  indicator["var"] = 0.11;
+  expect(level(indicator)).toBe("L");
+  indicator["var"] = 0.049;
+  expect(level(indicator)).toBe("H");
+});
+
+test("Returns correct level with direction 1", () => {
+  indicator["level_direction"] = 1;
+  indicator["level_green"] = 0.95;
+  indicator["level_yellow"] = 0.8;
+  expect(level(indicator)).toBe("L");
+  indicator["var"] = 0.85;
+  expect(level(indicator)).toBe("M");
+  indicator["var"] = 0.79;
+  expect(level(indicator)).toBe("L");
+  indicator["var"] = 0.945;
+  expect(level(indicator)).toBe("H");
+});
+
+test("Returns no level with direction null", () => {
+  indicator["level_direction"] = null;
+  indicator["level_green"] = 0.95;
+  indicator["level_yellow"] = 0.8;
+  expect(level(indicator)).toBe(undefined);
+});
