f662c6e8c553fbf137389ba2a510849cdb468f05
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-02-01 14:44:29 +0100

    Revert "Bruk 'level' i indikatordata, hvis den eksisterer (#748)" (#783)
    This reverts commit 7c7158be7960932365e4d9ed89840765481d1e68.

--- packages/qmongjs/src/helpers/functions/defineLevel.ts
+++ packages/qmongjs/src/helpers/functions/defineLevel.ts
@@ -5,11 +5,7 @@ const numWithValidDigits = (value: number, decimals: number) => {
 };
 
 export const level = (indicatorData: Indicator) => {
-  const { level, level_green, level_yellow, level_direction, sformat } =
-    indicatorData;
-  if (level !== undefined) {
-    return level;
-  }
+  const { level_green, level_yellow, level_direction, sformat } = indicatorData;
   if (
     level_green === undefined ||
     level_green === null ||
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -29,11 +29,10 @@ export interface Indicator {
   year: number;
   denominator: number;
   var: number;
-  level?: null | "H" | "M" | "L" | "";
   level_direction: number | null;
-  level_green?: number | null;
-  level_yellow?: number | null;
-  sformat?: string | null;
+  level_green: number | null;
+  level_yellow: number | null;
+  sformat: string | null;
   dg: number | null;
   delivery_time: Date | null;
   delivery_latest_update?: Date | null;
