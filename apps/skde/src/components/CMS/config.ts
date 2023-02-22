171a844016308c3bc3d2536d507cd6951c324b66
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-02-22 13:37:42 +0100

    Erstattet død CMS med aktiv fork (#795)
    Netlify-CMS ble ikke lenger utviklet. Dette vil på sikt føre til en del sårbarheter og bugs som ikke blir fikset. Static CMS er en fork av Netlify-CMS der blant annet koden er gjort om til typescript, bugs er fikset og avhengigheter oppdatert.
    
    Kopiert oppsett fra https://github.com/StaticJsCMS/static-cms-next-netlify-template, men med noen forenklinger.

--- apps/skde/src/components/CMS/config.ts
+++ apps/skde/src/components/CMS/config.ts
@@ -1,12 +1,12 @@
-import { CmsCollection, CmsConfig, CmsField } from "netlify-cms-core";
+import { Collection, Config, Field } from "@staticcms/core";
 
-const filename: CmsField = {
+const filename: Field = {
   label: "Filnavn",
   name: "filename",
   widget: "string",
 };
 
-const atlas = (lang: "no" | "en"): CmsCollection => {
+const atlas = (lang: "no" | "en"): Collection => {
   return {
     label: lang === "no" ? "Atlas" : "Engelske atlas",
     name: lang === "no" ? "atlas" : "atlas_eng",
@@ -187,7 +187,7 @@ const atlas = (lang: "no" | "en"): CmsCollection => {
   };
 };
 
-const staticPages = (lang: "no" | "en"): CmsCollection => {
+const staticPages = (lang: "no" | "en"): Collection => {
   return {
     label: lang === "no" ? "Statiske sider" : "Statiske engelske sider",
     name: lang === "no" ? "statiske_sider" : "statiske_sider_en",
@@ -219,13 +219,14 @@ const staticPages = (lang: "no" | "en"): CmsCollection => {
   };
 };
 
-export const config: CmsConfig = {
+export const config: Config = {
   local_backend: true,
   backend: {
     name: "github",
     repo: "mong/mongts",
     branch: "main",
-    base_url: "https://prod-mong-api.skde.org",
+    base_url:
+      process.env.NEXT_PUBLIC_API_HOST ?? "https://prod-mong-api.skde.org",
   },
   logo_url: "https://www.skde.no/helseatlas/img/logos/helseatlas.svg",
   media_folder: "/apps/skde/public/helseatlas/img",
@@ -233,4 +234,5 @@ export const config: CmsConfig = {
   site_url: "https://www.skde.no/helseatlas/",
   locale: "nb_no",
   collections: [atlas("no"), staticPages("no"), atlas("en"), staticPages("en")],
+  search: false,
 };
