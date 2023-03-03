6a9f7504926ebc7d98f6066522071f7f004efad5
Jørgen Reinnes <jorgenreinnes@gmail.com>
2023-03-03 21:10:59 +0100

    chore: basic indexing of atlas records

--- packages/indexer/src/methods/index-helseatlas.ts
+++ packages/indexer/src/methods/index-helseatlas.ts
@@ -1,8 +1,14 @@
 import algoliasearch from "algoliasearch";
-import * as env from "env";
+import * as env from "env-var";
 import { readdir, readFile } from "fs/promises";
 import { tryParseJSON } from "../utils";
 const HELSEATLAS_DIR = "../../apps/skde/_posts/helseatlas/atlas";
+
+const ALGOLIA_APP_ID = env.get("ALGOLIA_APP_ID").required(true).asString();
+const ALGOLIA_WRITE_KEY = env
+  .get("ALGOLIA_WRITE_KEY")
+  .required(true)
+  .asString();
 
 // These types are copied from the helseatlast code. They should be put in some common .ts file which this code is
 // allowed to import from
@@ -49,8 +55,6 @@ type AtlasJson = {
   publisert: boolean;
 };
 
-const APP_ID = env;
-
 const parseContent = (content: ChapterProps["innhold"][0]) => {
   switch (content.type) {
     case "faktaboks":
@@ -77,38 +81,70 @@ const parseContent = (content: ChapterProps["innhold"][0]) => {
 };
 
 const indexHelseatlas = async () => {
-  const atlasFiles = await readdir(HELSEATLAS_DIR);
-  const atlasRecords = await Promise.all(
-    atlasFiles.map(async (fileName) => {
-      const file = await readFile(`${HELSEATLAS_DIR}/${fileName}`);
-      try {
-        const res = tryParseJSON(file.toString());
-        if (res.error) {
-          return null;
-        }
-        const data: AtlasJson = res.parsed;
-        if (!data.publisert) {
-          return null;
-        }
-        const title = data.mainTitle;
-        return data.kapittel
-          .map((chapter) =>
-            chapter.innhold.map((paragraph) => ({
+  const atlasFiles = await readdir(HELSEATLAS_DIR); // ["ortopedi.json"]; //
+  const records = (
+    await Promise.all(
+      atlasFiles.map(async (fileName) => {
+        const file = await readFile(`${HELSEATLAS_DIR}/${fileName}`);
+        try {
+          const res = tryParseJSON(file.toString());
+          if (res.error) {
+            return [];
+          }
+          const data: AtlasJson = res.parsed;
+          if (!data.publisert) {
+            return [];
+          }
+          const title = data.mainTitle;
+          const permalink = `https://www.skde.no/helseatlas/v2/${data.filename}`;
+          const chapters = data.kapittel
+            .map((chapter) =>
+              chapter.innhold.map((paragraph) => ({
+                title,
+                chapterTitle: chapter.overskrift,
+                permalink,
+                // Group records on a per paragraph basis
+                distinctKey: `${chapter.overskrift}_${title}`,
+                content: parseContent(paragraph),
+              }))
+            )
+            .flat();
+          if (data.ingress) {
+            chapters.unshift({
               title,
-              chapterTitle: chapter.overskrift,
-              permalink: `https://www.skde.no/helseatlas/v2/${data.filename}`,
-              content: parseContent(paragraph),
-            }))
-          )
-          .flat();
-      } catch (e) {
-        console.error(`Failed to index ${fileName}`, e);
-      }
-    })
-  );
-  console.log(atlasRecords[0]);
-  const client = algoliasearch("APP_ID", "YourWriteAPIKey");
+              permalink,
+              chapterTitle: "Ingress",
+              distinctKey: `Ingress_${title}`,
+              content: {
+                content: data.ingress,
+              },
+            });
+          }
+          console.log(`Records created for ${fileName}`);
+          return chapters;
+        } catch (e) {
+          console.error(`Failed to index ${fileName}`, e);
+          return [];
+        }
+      })
+    )
+  ).flat();
+  const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_WRITE_KEY);
   const index = client.initIndex("prod_HELSEATLAS");
+  try {
+    await index.setSettings({
+      attributeForDistinct: "distinctKey",
+      distinct: true,
+    });
+  } catch (e) {
+    console.error("Failed to set index settings", e);
+  }
+  try {
+    await index.saveObjects(records, { autoGenerateObjectIDIfNotExist: true });
+    console.log("Pushed records to algolia");
+  } catch (e) {
+    console.error("Failed to push records to algolia", e);
+  }
   console.log(atlasFiles);
 };
 
