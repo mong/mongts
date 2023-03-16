e8312445d24aa3e92e3584c098fe090892dbaf56
arnfinn <arnfinn.steindal@gmail.com>
2023-03-16 16:54:34 +0100

    Inkludert url til sandbox der snapshots kan sees

--- apps/skde/src/charts/Table/__tests__/index.tsx
+++ apps/skde/src/charts/Table/__tests__/index.tsx
@@ -1,3 +1,11 @@
+/**
+ * To view a rendered version of all the snapshots:
+ *
+ * Copy the contents of ./__snapshots__/index.tsx.snap
+ * Go to https://codesandbox.io/s/snapshot-viewer-lqjjsm?file=/src/snapshots.js
+ * Replace the contents with what you copied
+ */
+
 import { render, fireEvent } from "@testing-library/react";
 import mockRouter from "next-router-mock";
 
