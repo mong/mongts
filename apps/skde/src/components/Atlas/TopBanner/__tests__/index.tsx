2ca547d02ebe8775fc352bdf40997640a423c993
arnfinn <arnfinn.steindal@gmail.com>
2023-03-20 13:18:23 +0100

    Test atlas topbanner-komponent

---
+++ apps/skde/src/components/Atlas/TopBanner/__tests__/index.tsx
@@ -0,0 +1,58 @@
+/**
+ * To view a rendered version of all the snapshots:
+ *
+ * Copy the contents of ./__snapshots__/index.tsx.snap
+ * Go to https://codesandbox.io/s/snapshot-viewer-lqjjsm?file=/src/snapshots.js
+ * Replace the contents with what you copied
+ */
+
+import { render, fireEvent } from "@testing-library/react";
+import { TopBanner } from "..";
+
+test("Bokmål render", async () => {
+  const title = "Dette er en tittel";
+  const { container } = render(
+    <TopBanner mainTitle={title} ia={false} lang="nb" />
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("Nynorsk render", async () => {
+  const title = "Dette er ein tittel";
+  const { container } = render(
+    <TopBanner mainTitle={title} ia={false} lang="nn" />
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("English render", async () => {
+  const title = "This is a title";
+  const { container } = render(
+    <TopBanner mainTitle={title} ia={false} lang="en" />
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("Random lang render", async () => {
+  const title = "Dette er ein tittel";
+  const { container } = render(
+    <TopBanner mainTitle={title} ia={false} lang="qwerty" />
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("IA render", async () => {
+  const title = "Dette er en tittel";
+  const { container } = render(
+    <TopBanner mainTitle={title} ia={true} lang="nb" />
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("IA english render", async () => {
+  const title = "Dette er en tittel";
+  const { container } = render(
+    <TopBanner mainTitle={title} ia={true} lang="en" />
+  );
+  expect(container).toMatchSnapshot();
+});
