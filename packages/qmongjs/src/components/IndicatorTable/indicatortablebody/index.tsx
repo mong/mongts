94fde2d143cd2580abc9b74e18d1b1990042571f
arnfinn <arnfinn.steindal@gmail.com>
2023-04-19 08:59:13 +0200

    import fix

--- packages/qmongjs/src/components/IndicatorTable/indicatortablebody/index.tsx
+++ packages/qmongjs/src/components/IndicatorTable/indicatortablebody/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { RegisterName } from "types";
-import TableBlock from "../tableblock/tableblock";
+import TableBlock from "../tableblock";
 import { NoDataAvailible } from "../ContenForEmptyTable";
 
 export interface IndicatorTableBodyProps {
