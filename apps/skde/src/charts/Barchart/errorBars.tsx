745deefb919ac822d1f5c8407879964e4dd01983
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-05-22 08:36:40 +0200

    errorbars i barchart (#1098)
    Settes på figur hvis errorBars er gitt i data-fila.

---
+++ apps/skde/src/charts/Barchart/errorBars.tsx
@@ -0,0 +1,49 @@
+import { Group } from "@visx/group";
+import { ScaleBand, ScaleLinear } from "d3-scale";
+import { max, min } from "d3-array";
+
+type ErrorBarProps<Data, ErrorBar> = {
+  data: Data;
+  y: keyof Data;
+  errorBars: ErrorBar;
+  xScale: ScaleLinear<number, number, never>;
+  yScale: ScaleBand<string>;
+};
+
+export const ErrorBars = function <D, ErrorBar extends (string & keyof D)[]>({
+  data,
+  xScale,
+  yScale,
+  errorBars,
+  y,
+}: ErrorBarProps<D, ErrorBar>) {
+  const allBars = errorBars.map((v) => Number(data[v]));
+
+  return (
+    <>
+      <Group top={yScale.bandwidth() / 2}>
+        <line
+          x1={xScale(min(allBars))}
+          x2={xScale(max(allBars))}
+          y1={yScale(data[y].toString())}
+          y2={yScale(data[y].toString())}
+          stroke={"black"}
+          strokeWidth="2"
+        />
+        {[...errorBars].map((d) => {
+          return (
+            <line
+              x1={xScale(Number(data[d]))}
+              x2={xScale(Number(data[d]))}
+              y1={yScale(data[y].toString()) + 3}
+              y2={yScale(data[y].toString()) - 3}
+              stroke={"black"}
+              strokeWidth="2"
+              key={`${d}_errorbar`}
+            />
+          );
+        })}
+      </Group>
+    </>
+  );
+};
