833842c2ef6415783de7f50eb5855b3d6b720dd9
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-05-23 13:04:34 +0200

    lang tag i HTML head. (#1103)
    * lang tag i HTML head.
    
    Prøver med løsningen foreslått her: https://stackoverflow.com/a/62361243
    
    Closes #1088
    
    Prøvd løst i #1090 og #1094 tidligere, uten hell.

---
+++ apps/skde/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document<{ lang: string }> {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const { pathname } = ctx;
+    const lang =
+      pathname.includes("/en/") || pathname.endsWith("/en") ? "en" : "no";
+    return { ...initialProps, lang };
+  }
+
+  render() {
+    const { lang } = this.props;
+    return (
+      <Html lang={lang}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
