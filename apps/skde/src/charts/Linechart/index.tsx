21717bb010ee6fb02bf8a8245e62693baf1098e2
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-06-07 08:30:59 +0200

    Utvikling linechart (#1165)
    * Format på y-akse
    
    * Litt mer høyde
    
    * Måned på x-aksen
    
    * y-akse-tekst på toppen
    
    * Format på tooltip og akser
    
    * Info om alle labels i tooltip
    
    * lik font-size på x-akse-tall

--- apps/skde/src/charts/Linechart/index.tsx
+++ apps/skde/src/charts/Linechart/index.tsx
@@ -1,4 +1,8 @@
 import { Grid, Axis, LineSeries, XYChart, Tooltip } from "@visx/xychart";
+import {
+  customFormat,
+  customFormatEng,
+} from "../../helpers/functions/localFormater";
 
 export type LinechartData<
   Data,
@@ -28,6 +32,8 @@ type LinechartProps<
   lang: "en" | "nb" | "nn";
   xLabel?: { en: string; nb: string; nn: string };
   yLabel?: { en: string; nb: string; nn: string };
+  format_x?: string;
+  format_y?: string;
 };
 
 export const Linechart = <
@@ -43,6 +49,8 @@ export const Linechart = <
   lang,
   xLabel,
   yLabel,
+  format_x,
+  format_y,
 }: LinechartProps<Data, X, Y, Label>) => {
   let uniqueLabels = Array.from(new Set(data.map((d) => d[label])));
 
@@ -62,14 +70,22 @@ export const Linechart = <
   });
 
   const accessors = {
-    xAccessor: (d) => d.x,
-    yAccessor: (d) => d.y,
+    xAccessor: (d) =>
+      format_x === "month"
+        ? new Date(2020, d.x - 1).toLocaleString(lang, { month: "long" })
+        : d.x,
+    yAccessor: (d) =>
+      format_y
+        ? lang === "en"
+          ? customFormatEng(format_y)(d.y)
+          : customFormat(format_y)(d.y)
+        : d.y,
   };
 
   return (
     <div style={{ width: "auto", margin: "auto" }}>
       <XYChart
-        height={300}
+        height={500}
         xScale={{ type: "band" }}
         yScale={{ type: "linear" }}
       >
@@ -81,6 +97,16 @@ export const Linechart = <
             textAnchor: "middle",
             fontWeight: "bold",
           }}
+          tickLabelProps={() => ({
+            fontSize: 14,
+            fill: "black",
+          })}
+          tickFormat={(val) =>
+            format_x === "month"
+              ? new Date(2020, val - 1).toLocaleString(lang, { month: "short" })
+              : val
+          }
+          numTicks={11}
         />
         <Axis
           orientation="left"
@@ -93,11 +119,20 @@ export const Linechart = <
           label={yLabel[lang]}
           labelProps={{
             fontSize: 14,
-            textAnchor: "middle",
+            textAnchor: "start",
             fontWeight: "bold",
-            y: -35,
+            x: -50,
+            y: 25,
+            transform: "",
           }}
           stroke="black"
+          tickFormat={(val) =>
+            format_y
+              ? lang === "en"
+                ? customFormatEng(format_y)(val)
+                : customFormat(format_y)(val)
+              : val.toString()
+          }
         />
         <Grid columns={false} numTicks={4} />
         {values.map((plots, i) => (
@@ -116,12 +151,22 @@ export const Linechart = <
           showSeriesGlyphs
           renderTooltip={({ tooltipData, colorScale }) => (
             <div>
-              <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
-                {tooltipData.nearestDatum.key}
+              <div>
+                {xLabel[lang].split(/[^A-Za-zæøåÆØÅ]/)[0]}
+                {": "}
+                {accessors.xAccessor(tooltipData.nearestDatum.datum)}
               </div>
-              {accessors.xAccessor(tooltipData.nearestDatum.datum)}
-              {", "}
-              {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+              {Object.keys(tooltipData.datumByKey).map((d) => {
+                return (
+                  <div key={d}>
+                    <div style={{ color: colorScale(d) }}>
+                      {d}
+                      {": "}
+                      {accessors.yAccessor(tooltipData.datumByKey[d].datum)}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           )}
         />
