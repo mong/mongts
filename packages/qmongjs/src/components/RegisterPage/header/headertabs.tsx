b2e3c2e45eff1d95e7a9e8232ad97240c993c5fc
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-06-21 08:42:14 +0200

    Behold alle queryparams etter bytte av fane (#1216)

--- packages/qmongjs/src/components/RegisterPage/header/headertabs.tsx
+++ packages/qmongjs/src/components/RegisterPage/header/headertabs.tsx
@@ -43,6 +43,8 @@ const Tab: React.FC<TabProps> = ({ tabName, activeTab }) => {
     register: string;
   };
 
+  const queries = useRouter().asPath.split("?")[1];
+  const queryPath = queries ? `?${queries}` : "";
   const clickedStyle =
     activeTab === tabName.value
       ? {
@@ -60,7 +62,7 @@ const Tab: React.FC<TabProps> = ({ tabName, activeTab }) => {
   return (
     <li className={style.tabsLI}>
       <Link
-        href={`${path}/${tabName.value}`}
+        href={`${path}/${tabName.value}/${queryPath}`}
         passHref
         role="tab"
         aria-selected={tabName.value === tab}
