4bdd36eafcb948cf7142faf01b2083c45b7be428
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-09-22 15:07:35 +0200

    Behold 'vis/skjul målnivå'-valg i alle figurer (#1213)

--- packages/qmongjs/src/app_config.ts
+++ packages/qmongjs/src/app_config.ts
@@ -2,6 +2,7 @@ import {
   DelimitedArrayParam,
   NumberParam,
   StringParam,
+  BooleanParam,
   withDefault,
 } from "use-query-params";
 
@@ -51,4 +52,5 @@ export const mainQueryParamsConfig = {
   year: withDefault(NumberParam, undefined),
   selected_treatment_units: withDefault(DelimitedArrayParam, undefined),
   chart_type: withDefault(StringParam, undefined),
+  chart_show_level: withDefault(BooleanParam, undefined),
 };
