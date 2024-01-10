205d074b57a2fc1e723cbc3ec5090cc3d36affc3
arnfinn <arnfinn.steindal@gmail.com>
2024-01-10 12:48:31 +0100

    Aktiver mulighet for å velge 2023 i rapportalen

--- packages/qmongjs/src/app_config.ts
+++ packages/qmongjs/src/app_config.ts
@@ -43,7 +43,7 @@ export const app_text: appTextTypes = {
 };
 
 export const minYear = 2017;
-export const maxYear = 2022;
+export const maxYear = 2023;
 export const defaultYear = 2022;
 export const mainQueryParamsConfig = {
   selected_row: withDefault(StringParam, undefined),
