6330fe2f6fc05c4b115c8ff90f3d93555553486f
jo-inge-arnes <jo.inge.arnes@helse-nord.no>
2024-02-09 14:27:33 +0100

    added attribute for onSelectionChanged and a wrapper for reducer calling this handler

--- packages/qmongjs/src/components/FilterMenu/FilterSettingsContext.ts
+++ packages/qmongjs/src/components/FilterMenu/FilterSettingsContext.ts
@@ -1,26 +1,42 @@
-import React, { createContext } from 'react';
+import React, { createContext } from "react";
 
-export type FilterSettingsValue = { valueId: string, value: string };
+export type FilterSettingsValue = { valueId: string; value: string };
 
 export type FilterSettings = Map<string, FilterSettingsValue[]>;
 export type FilterSettingsAction = {
-    type: FilterSettingsActionType;
-    sectionSetting: { key: string, values: FilterSettingsValue[] };
+  type: FilterSettingsActionType;
+  sectionSetting: { key: string; values: FilterSettingsValue[] };
 };
 
-export enum FilterSettingsActionType { NOT_SET, SET_SECTION_SELECTIONS };
-
-export function filterSettingsReducer(filterSettings: FilterSettings, action: FilterSettingsAction) {
-    switch (action.type) {
-        case FilterSettingsActionType.SET_SECTION_SELECTIONS:
-            const newFilterSettings = new Map<string, FilterSettingsValue[]>(filterSettings.entries());
-            newFilterSettings.set(action.sectionSetting.key, action.sectionSetting.values);
-            return newFilterSettings;
-
-        default:
-            return filterSettings;
-    }
+export enum FilterSettingsActionType {
+  NOT_SET,
+  SET_SECTION_SELECTIONS,
 }
 
-export const FilterSettingsContext = createContext<FilterSettings>(new Map<string, FilterSettingsValue[]>());
-export const FilterSettingsDispatchContext = createContext<React.Dispatch<FilterSettingsAction>>(() => {});
+export function filterSettingsReducer(
+  filterSettings: FilterSettings,
+  action: FilterSettingsAction,
+) {
+  switch (action.type) {
+    case FilterSettingsActionType.SET_SECTION_SELECTIONS: {
+      const newFilterSettings = new Map<string, FilterSettingsValue[]>(
+        filterSettings.entries(),
+      );
+      newFilterSettings.set(
+        action.sectionSetting.key,
+        action.sectionSetting.values,
+      );
+      return newFilterSettings;
+    }
+
+    default:
+      return filterSettings;
+  }
+}
+
+export const FilterSettingsContext = createContext<FilterSettings>(
+  new Map<string, FilterSettingsValue[]>(),
+);
+export const FilterSettingsDispatchContext = createContext<
+  React.Dispatch<FilterSettingsAction>
+>(() => {});
