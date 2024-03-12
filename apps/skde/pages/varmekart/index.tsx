8698eabff5319a1902211cc27f1f3c36e85f8344
stein-kato <steinkato.lindberg@yahoo.no>
2024-03-12 10:30:42 +0100

    Skall

---
+++ apps/skde/pages/varmekart/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeatMap from "../../src/charts/HeatMap";
+
+export const Skde = (): JSX.Element => {
+  return (
+    <div>
+      <HeatMap></HeatMap>
+    </div>
+  );
+};
+
+export default Skde;
---
+++ apps/skde/src/charts/HeatMap/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { HeatMapRect } from "@visx/heatmap";
+
+type HeatMapProps = {
+  hospitals: string[];
+  indicators: string[];
+};
+
+export default function HeatMap(props: HeatMapProps) {
+  return <div></div>;
+}
