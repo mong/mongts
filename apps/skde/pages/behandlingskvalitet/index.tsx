2f97418d8fcd35363af8f3dde45ae19d162284b7
Jo Inge Arnes <jo.inge.arnes@live.no>
2024-03-13 16:27:36 +0100

    only permanently displayed drawer for viewport widths of 1300px and above (#2144)
    Co-authored-by: jo-inge-arnes <jo.inge.arnes@helse-nord.no>

--- apps/skde/pages/behandlingskvalitet/index.tsx
+++ apps/skde/pages/behandlingskvalitet/index.tsx
@@ -1,7 +1,6 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import IconButton from "@mui/material/IconButton";
 import TuneIcon from "@mui/icons-material/Tune";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -10,13 +9,16 @@ import { imgLoader } from "qmongjs/src/helpers/functions";
 import { useState, useEffect } from "react";
 import { TreatmentQualityFilterMenu } from "qmongjs";
 import {
+  FilterIconButton,
   FilterDrawer,
   FilterDrawerBox,
   MainBox,
   appBarElevation,
   filterMenuTopMargin,
-  smSizeWidth,
+  desktopBreakpoint,
+  treatmentQualityTheme,
 } from "../../src/components/TreatmentQuality";
+import { ThemeProvider } from "@mui/material/styles";
 
 /**
  * Treatment quality page (Behandlingskvalitet)
@@ -24,12 +26,12 @@ import {
  * @returns The page component
  */
 export default function TreatmentQuality() {
-  const [width, setWidth] = useState(smSizeWidth);
+  const [width, setWidth] = useState(desktopBreakpoint);
   const [mobileOpen, setMobileOpen] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
-  const isSmallScreen = width <= smSizeWidth;
-  const drawerOpen = isSmallScreen ? mobileOpen : true;
-  const drawerType = isSmallScreen ? "temporary" : "permanent";
+  const isPhoneSizedScreen = width < desktopBreakpoint;
+  const drawerOpen = isPhoneSizedScreen ? mobileOpen : true;
+  const drawerType = isPhoneSizedScreen ? "temporary" : "permanent";
 
   // Used to change drawer style between small screens and larger screens
   useEffect(() => {
@@ -64,82 +66,83 @@ export default function TreatmentQuality() {
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        elevation={appBarElevation}
-        sx={{
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-          backgroundColor: "rgba(222, 231, 238, 1)",
-          color: "black",
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="åpne sidemeny"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <TuneIcon />
-            <Typography>Filter</Typography>
-          </IconButton>
-          <Box sx={{ marginLeft: 2 }}>
-            <Typography variant="h6">Behandlingskvalitet</Typography>
-          </Box>
-          <Box sx={{ marginLeft: "auto", marginTop: 1.15 }}>
-            <Image
-              loader={imgLoader}
-              src="/img/logos/SKDE_sort.png"
-              height="40"
-              width="99"
-              alt="SKDE logo"
-            />
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <FilterDrawerBox
-        component="nav"
-        sx={{ flexShrink: { sm: 0 } }}
-        aria-label="filtermenyboks"
-      >
-        <Toolbar />
-        <FilterDrawer
-          variant={drawerType}
-          open={drawerOpen}
-          onTransitionEnd={handleDrawerTransitionEnd}
-          onClose={handleDrawerClose}
-          ModalProps={{
-            keepMounted: true,
-          }}
+    <ThemeProvider theme={treatmentQualityTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          elevation={appBarElevation}
           sx={{
-            display: "block",
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-            },
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            backgroundColor: "rgba(222, 231, 238, 1)",
+            color: "black",
+          }}
+        >
+          <Toolbar>
+            <FilterIconButton
+              color="inherit"
+              aria-label="åpne sidemeny"
+              edge="start"
+              onClick={handleDrawerToggle}
+            >
+              <TuneIcon />
+              <Typography>Filter</Typography>
+            </FilterIconButton>
+            <Box sx={{ marginLeft: 2 }}>
+              <Typography variant="h6">Behandlingskvalitet</Typography>
+            </Box>
+            <Box sx={{ marginLeft: "auto", marginTop: 1.15 }}>
+              <Image
+                loader={imgLoader}
+                src="/img/logos/SKDE_sort.png"
+                height="40"
+                width="99"
+                alt="SKDE logo"
+              />
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <FilterDrawerBox
+          component="nav"
+          sx={{ flexShrink: { sm: 0 } }}
+          aria-label="filtermenyboks"
+        >
+          <Toolbar />
+          <FilterDrawer
+            variant={drawerType}
+            open={drawerOpen}
+            onTransitionEnd={handleDrawerTransitionEnd}
+            onClose={handleDrawerClose}
+            ModalProps={{
+              keepMounted: true,
+            }}
+            sx={{
+              display: "block",
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+              },
+            }}
+          >
+            <Toolbar />
+            <Box sx={{ marginTop: filterMenuTopMargin }}>
+              <TreatmentQualityFilterMenu />
+            </Box>
+          </FilterDrawer>
+        </FilterDrawerBox>
+        <MainBox
+          component="main"
+          sx={{
+            paddingLeft: "0px",
+            flexGrow: 1,
+            p: 3,
           }}
         >
           <Toolbar />
-          <Box sx={{ marginTop: filterMenuTopMargin }}>
-            <TreatmentQualityFilterMenu />
-          </Box>
-        </FilterDrawer>
-      </FilterDrawerBox>
-      <MainBox
-        component="main"
-        sx={{
-          paddingLeft: "0px",
-          flexGrow: 1,
-          p: 3,
-        }}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          Resultater fra medisinske kvalitetsregistre
-        </Typography>
-      </MainBox>
-    </Box>
+          <Typography paragraph>
+            Resultater fra medisinske kvalitetsregistre
+          </Typography>
+        </MainBox>
+      </Box>
+    </ThemeProvider>
   );
 }
