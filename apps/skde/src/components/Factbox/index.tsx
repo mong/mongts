6a10659334fc517e1198f8c7c613615967e0ff0d
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-03-21 12:59:56 +0100

    Fjernet unødvendig expanded state variabel

--- apps/skde/src/components/Factbox/index.tsx
+++ apps/skde/src/components/Factbox/index.tsx
@@ -19,7 +19,6 @@ export const FactBox: React.FC<FactBoxProps> = ({
   id,
   lang,
 }) => {
-  const [expanded, setExpanded] = React.useState<boolean>(false);
   return (
     <div style={{ marginBottom: "10px" }} data-testid="factbox">
       <Accordion
@@ -27,8 +26,6 @@ export const FactBox: React.FC<FactBoxProps> = ({
           boxShadow: 1,
           borderBottom: "2px solid #033F85",
         }}
-        expanded={expanded}
-        onChange={() => setExpanded(!expanded)}
       >
         <AccordionSummary
           sx={{
