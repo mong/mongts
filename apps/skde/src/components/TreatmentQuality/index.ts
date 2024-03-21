968ea93bd049f6354327e7dc96623dad08fb3ee5
Jo Inge Arnes <jo.inge.arnes@live.no>
2024-03-21 15:19:03 +0100

    2187 hindre at footer vises mens indikatortabell laster (#2195)
    * making indicator columns narrower for small screens. Removing some padding for header to reduce the height.
    
    * moving footer down, and some styling

--- apps/skde/src/components/TreatmentQuality/index.ts
+++ apps/skde/src/components/TreatmentQuality/index.ts
@@ -137,6 +137,7 @@ export const SkdeLogoBox = styled(Box)(({ theme }) => ({
 }));
 
 export const FooterWrapper = styled(Paper)(({ theme }) => ({
+  marginTop: "calc(100vh)",
   color: "white",
 }));
 
@@ -181,15 +182,16 @@ export const MainBox = styled(Box)(({ theme }) => ({
     top: "0px !important",
     position: "sticky",
     zIndex: theme.zIndex.drawer - 1,
-    minWidth: "128px",
+    minWidth: "10rem",
     verticalAlign: "top",
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingRight: 15,
+    lineHeight: "1.5rem",
     backgroundColor: "#00263d",
     color: "white",
   },
   [theme.breakpoints.down("sm")]: {
+    "& th": {
+      minWidth: "6rem",
+    },
     marginTop: "56px",
     height: "calc(100vh - 56px)",
     "& .MuiDrawer-paper": {
