1c7b96f7190d01b987ef010c89703b013b869403
jo-inge-arnes <jo.inge.arnes@helse-nord.no>
2024-04-03 17:20:26 +0200

    checking links and printing urls having non-ok status-codes.

--- scripts/atlas/checklinks.py
+++ scripts/atlas/checklinks.py
@@ -3,6 +3,8 @@
 import sys
 import os
 import fnmatch
+import re
+import requests
 
 
 def get_matching_filenames(directory, pattern):
@@ -14,12 +16,41 @@ def get_matching_filenames(directory, pattern):
     return matching_filenames
 
 
+def extract_urls(text):
+    url_pattern = r'\((http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+?)\)+'
+    urls = re.findall(url_pattern, text)
+    return urls
+
+
+def check_link(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0',
+            'Referer': 'https://www.skde.no/'
+        }
+        response = requests.get(url, headers=headers)
+        status_code = response.status_code
+        if status_code != 200:
+            print(f"Non-200 status code for {url}: {status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error accessing {url}: {e}")
+
+
+def process_file(filename):
+    # Your code to process each file goes here
+    with open(filename, 'r') as file:
+        file_content = file.read()
+        print(f"Processing file: {filename}")
+        urls = extract_urls(file_content)
+        for url in urls:
+            check_link(url)
+
 def main(search_dir, file_pattern='*.json'):
     script_dir = os.path.dirname(os.path.realpath(__file__))  # Get the directory name where the file is located
     directory = os.path.join(script_dir, search_dir)
-    print(f"Searching for files matching pattern '{file_pattern}' in directory '{directory}'")
     matching_files = get_matching_filenames(directory, file_pattern)
-    print(matching_files)
+    for filename in matching_files:
+        process_file(filename)
 
 
 if __name__ == "__main__":
@@ -28,4 +59,4 @@ if __name__ == "__main__":
         sys.exit(1)
     search_dir = sys.argv[1]
     file_pattern = sys.argv[2] if len(sys.argv) > 2 else '*.json' # Add file_pattern as an optional argument
-    main(search_dir, file_pattern) # Pass file_pattern to main
+    main(search_dir, file_pattern)
