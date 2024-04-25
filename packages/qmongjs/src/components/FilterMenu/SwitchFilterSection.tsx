9816aae055486d0aa79067a3e64b12aae7a864d6
Jo Inge Arnes <jo.i.arnes@uit.no>
2024-04-25 13:36:09 +0200

    Added query parameter and switch component for data quality (datakvalitet)

---
+++ packages/qmongjs/src/components/FilterMenu/SwitchFilterSection.tsx
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import { FormControlLabel, FormGroup, Switch } from "@mui/material";
+import { FilterMenuSectionProps } from ".";
+import {
+  FilterSettingsContext,
+  FilterSettingsValue,
+} from "./FilterSettingsContext";
+import { FilterSettingsDispatchContext } from "./FilterSettingsReducer";
+import { FilterSettingsActionType } from "./FilterSettingsReducer";
+import { getSelectedValue } from "./utils";
+
+export type SwitchFilterSectionProps = FilterMenuSectionProps & {
+  label: string;
+  activatedswitchvalue: FilterSettingsValue;
+};
+
+export function SwitchFilterSection(props: SwitchFilterSectionProps) {
+  const labelText = props.label;
+  const filterKey = props.filterkey;
+  const activatedSwitchValue = props.activatedswitchvalue;
+  const filterSettings = useContext(FilterSettingsContext);
+  const filterSettingsDispatch = useContext(FilterSettingsDispatchContext);
+
+  const selectedValue = getSelectedValue(filterKey, filterSettings);
+  const isChecked: boolean = selectedValue === "true";
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (filterSettings) {
+      filterSettingsDispatch({
+        type: event.target.checked
+          ? FilterSettingsActionType.SET_SECTION_SELECTIONS
+          : FilterSettingsActionType.DEL_SECTION_SELECTIONS,
+        sectionSetting: {
+          key: filterKey,
+          values: [activatedSwitchValue],
+        },
+      });
+    }
+  };
+
+  return (
+    <FormGroup aria-label={props.label} row>
+      <FormControlLabel
+        labelPlacement="start"
+        label={labelText}
+        control={
+          <Switch checked={isChecked} onChange={handleChange} color="primary" />
+        }
+      />
+    </FormGroup>
+  );
+}
+
+export default SwitchFilterSection;
