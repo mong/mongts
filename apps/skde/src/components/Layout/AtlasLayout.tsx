a518681a15c3d27907d71bbb4e70c5f4a855e7b2
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-05-07 14:40:44 +0200

    Fjernet en linje for å tilfredstille eslint

--- apps/skde/src/components/Layout/AtlasLayout.tsx
+++ apps/skde/src/components/Layout/AtlasLayout.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react";
 import { LayoutHead } from "./LayoutHead";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
 interface Props {
   children: React.ReactNode;
-  title?: string;
   page?: string;
   lang: "no" | "en";
 }
