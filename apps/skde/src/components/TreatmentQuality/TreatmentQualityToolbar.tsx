0cd1f8020a6428e13a7a8aeb4e96362216c1e4a4
Jo Inge Arnes <jo.i.arnes@uit.no>
2024-05-13 12:29:15 +0200

    updated styles and layout

---
+++ apps/skde/src/components/TreatmentQuality/TreatmentQualityToolbar.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { Button, Tab, Tabs, Toolbar, styled } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.light,
+  color: theme.palette.primary.dark,
+  paddingTop: theme.spacing(2),
+  paddingBottom: theme.spacing(0),
+}));
+
+const StyledTabs = styled(Tabs)(({ theme }) => ({
+  "& .Mui-selected": {
+    backgroundColor: theme.palette.background.paper,
+  },
+  "& .MuiTabs-indicator": {
+    display: "none",
+  },
+}));
+
+const StyledTab = styled(Tab)(({ theme }) => ({
+  font: theme.typography.button.font,
+  textTransform: "none",
+  borderTopLeftRadius: 12,
+  borderTopRightRadius: 12,
+  fontWeight: "500",
+  paddingLeft: theme.spacing(4),
+  paddingRight: theme.spacing(4),
+}));
+
+type StickyToolbarProps = {
+  openDrawer: () => void;
+  context;
+  onTabChanged;
+};
+
+export const TreatmentQualityToolbar = ({
+  openDrawer,
+  context,
+  onTabChanged,
+}: StickyToolbarProps) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("narrow"));
+
+  const [value, setValue] = useState(
+    context === "resident" ? "resident" : "caregiver",
+  );
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+    setTimeout(() => {
+      onTabChanged(newValue);
+    }, 0);
+  };
+
+  return (
+    <StyledToolbar>
+      <Grid container spacing={4} columns={{ xs: 4, sm: 8, md: 12 }}>
+        <Grid xs={1} sm={2} md={2} alignContent="center">
+          <Button
+            variant="contained"
+            aria-label="Åpne sidemeny"
+            color="primary"
+            onClick={() => {
+              openDrawer();
+            }}
+          >
+            <Menu fontSize="medium" />
+          </Button>
+        </Grid>
+        <Grid xs={3} sm={6} md={10}>
+          <StyledTabs
+            aria-label="Arkfaner for behandlingskvalitet og opptaksområde"
+            value={value}
+            onChange={handleChange}
+            orientation={isSmallScreen ? "vertical" : "horizontal"}
+            variant="fullWidth"
+          >
+            <StyledTab label="Behandlingsenheter" value={"caregiver"} />
+            <StyledTab label="Opptaksområder" value={"resident"} />
+          </StyledTabs>
+        </Grid>
+      </Grid>
+    </StyledToolbar>
+  );
+};
