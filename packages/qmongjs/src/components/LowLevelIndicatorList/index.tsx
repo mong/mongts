183e9d77e99d02bc959d3da12b53e4a40976db7a
stein-kato <steinkato.lindberg@yahoo.no>
2024-05-21 13:34:21 +0200

    Fjernet print

--- packages/qmongjs/src/components/LowLevelIndicatorList/index.tsx
+++ packages/qmongjs/src/components/LowLevelIndicatorList/index.tsx
@@ -142,7 +142,7 @@ export const LowLevelIndicatorList = (props: LowLevelIndicatorListProps) => {
   // UI stuff
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [setting, setSetting] = React.useState("last-year");
-  console.log(props);
+
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
