682c3425b0fbc101839d92f4c0f563efaeaa1cbe
stein-kato <steinkato.lindberg@yahoo.no>
2024-05-27 11:00:05 +0200

    Bug-fiks

--- apps/skde/src/components/MedfieldTable/index.tsx
+++ apps/skde/src/components/MedfieldTable/index.tsx
@@ -8,7 +8,6 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 import { UseQueryResult } from "@tanstack/react-query";
 import { useIndicatorQuery } from "qmongjs";
