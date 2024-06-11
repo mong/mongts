97fe2b1b50424b3071932158e1da2382774292b3
stein-kato <steinkato.lindberg@yahoo.no>
2024-06-11 12:07:17 +0200

    Knapp

--- apps/skde/src/components/TreatmentQuality/TreatmentQualityHeaderMiddle.tsx
+++ apps/skde/src/components/TreatmentQuality/TreatmentQualityHeaderMiddle.tsx
@@ -1,5 +1,6 @@
 import { Toolbar, Typography, styled } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
+import Button from "@mui/material/Button";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   backgroundColor: theme.palette.primary.light,
@@ -18,6 +19,17 @@ export const HeaderMiddleToolbar = () => {
           <Typography variant="h6">
             Resultater fra nasjonale medisinske kvalitetsregistre
           </Typography>
+        </Grid>
+        <Grid container xs={4} sm={8} md={12} lg={12} justifyContent="flex-end">
+          <Grid xs={4} sm={4} md={4} lg={4}>
+            <Button
+              href="https://www.kvalitetsregistre.no/"
+              target="_blank"
+              variant="contained"
+            >
+              Mer om kvalitetsregistre
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </StyledToolbar>
