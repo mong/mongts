ab8cac77ec99d5497675b60e0b29d1467a8190c8
stein-kato <steinkato.lindberg@yahoo.no>
2024-06-12 09:04:14 +0200

    Piler i linker

--- apps/skde/src/components/ArrowLink/index.tsx
+++ apps/skde/src/components/ArrowLink/index.tsx
@@ -1,22 +1,34 @@
 import Button from "@mui/material/Button";
 import { ArrowForward, ArrowOutward } from "@mui/icons-material";
+import { Stack } from "@mui/material";
 
 type ArrowLinkProps = {
   href: string;
   text: string;
   diagonalArrow?: boolean;
+  button?: boolean;
 };
 
 export const ArrowLink = (props: ArrowLinkProps) => {
-  const { href, text, diagonalArrow } = props;
+  const { href, text, diagonalArrow, button } = props;
 
   let arrow: JSX.Element;
+  let align: string;
 
   diagonalArrow ? (arrow = <ArrowOutward />) : (arrow = <ArrowForward />);
+  diagonalArrow ? (align = "normal") : (align = "center");
 
-  return (
+  return button ? (
     <Button href={href} target="_blank" variant="text">
-      {[text, arrow]}
+      <Stack alignItems={align} direction="row" gap={1}>
+        {text}
+        {arrow}
+      </Stack>
     </Button>
+  ) : (
+    <Stack alignItems={align} direction="row" gap={1}>
+      {text}
+      {arrow}
+    </Stack>
   );
 };
