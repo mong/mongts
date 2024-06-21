b4ecf6a2a450d553d050979620b5d735ce5ddbe0
stein-kato <steinkato.lindberg@yahoo.no>
2024-06-21 10:47:41 +0200

    Gjenbruk av PageWrapper fra behandlingskvalitet

--- apps/skde/src/components/Layout/AtlasLayout.tsx
+++ apps/skde/src/components/Layout/AtlasLayout.tsx
@@ -1,35 +1,13 @@
 import { LayoutHead } from "./LayoutHead";
 import { Header } from "./Header";
 import { Footer } from "../Footer";
-import { Box, styled } from "@mui/material";
+import { PageWrapper } from "../StyledComponents/PageWrapper";
 
 interface Props {
   children: React.ReactNode;
   page?: string;
   lang: "no" | "en";
 }
-
-const PageWrapper = styled(Box)(({ theme }) => ({
-  "& .footer": {
-    [theme.breakpoints.down("sm")]: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2),
-    },
-    [theme.breakpoints.up("sm")]: {
-      paddingLeft: theme.spacing(4),
-      paddingRight: theme.spacing(4),
-    },
-    [theme.breakpoints.up("lg")]: {
-      paddingLeft: theme.spacing(6),
-      paddingRight: theme.spacing(6),
-    },
-    [theme.breakpoints.up("xl")]: {
-      paddingLeft: theme.spacing(16),
-      paddingRight: theme.spacing(16),
-    },
-  },
-  backgroundColor: theme.palette.background.paper,
-}));
 
 export function AtlasLayout({ children, lang }: Props) {
   return (
