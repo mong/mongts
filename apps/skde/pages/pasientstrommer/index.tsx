ab40fa4487200cd079139294a195eb354822a060
stein-kato <steinkato.lindberg@yahoo.no>
2024-06-21 15:55:53 +0200

    Footer og theming

--- apps/skde/pages/pasientstrommer/index.tsx
+++ apps/skde/pages/pasientstrommer/index.tsx
@@ -1,23 +1,32 @@
 import { FrontPageLayout } from "../../src/components/Layout";
 import IframeResizer from "iframe-resizer-react";
 import styles from "../../src/styles/Pages.module.css";
+import { Footer } from "../../src/components/Footer";
+import { ThemeProvider } from "@mui/material";
+import { skdeTheme } from "qmongjs";
+import { PageWrapper } from "../../src/components/StyledComponents/PageWrapper";
 
 const Pasient = () => {
   return (
-    <FrontPageLayout page="Pasientstrømmer">
-      <div className={styles.container_mod}>
-        <div className={styles.article}>
-          <div className={styles.article__title}>
-            <h2>Pasientstrømmer, Helse Nord RHF</h2>
+    <ThemeProvider theme={skdeTheme}>
+      <FrontPageLayout page="Pasientstrømmer">
+        <div className={styles.container_mod}>
+          <div className={styles.article}>
+            <div className={styles.article__title}>
+              <h2>Pasientstrømmer, Helse Nord RHF</h2>
+            </div>
           </div>
         </div>
-      </div>
-      <IframeResizer
-        log
-        src="https://prod-tabellverk.skde.org/"
-        style={{ width: "1px", minWidth: "100%", border: "none" }}
-      />
-    </FrontPageLayout>
+        <IframeResizer
+          log
+          src="https://prod-tabellverk.skde.org/"
+          style={{ width: "1px", minWidth: "100%", border: "none" }}
+        />
+      </FrontPageLayout>
+      <PageWrapper>
+        <Footer page="pasientstrømmer" />
+      </PageWrapper>
+    </ThemeProvider>
   );
 };
 export default Pasient;
