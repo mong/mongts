e3c0e0dedda4a9c0c02a82e8d03924eb1c713bc4
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2024-06-23 15:47:50 +0200

    Byttet iframe-resizer-bibliotek (#2709)
    iframe-resizer-react er nå en del av iframe-resizer-pakken.
    
    Vi bruker den til å pakke inn pasientstrøm-siden.

--- apps/skde/pages/pasientstrommer/index.tsx
+++ apps/skde/pages/pasientstrommer/index.tsx
@@ -1,5 +1,5 @@
 import { FrontPageLayout } from "../../src/components/Layout";
-import IframeResizer from "iframe-resizer-react";
+import IframeResizer from "@iframe-resizer/react";
 import styles from "../../src/styles/Pages.module.css";
 
 const Pasient = () => {
@@ -13,9 +13,14 @@ const Pasient = () => {
         </div>
       </div>
       <IframeResizer
-        log
         src="https://prod-tabellverk.skde.org/"
-        style={{ width: "1px", minWidth: "100%", border: "none" }}
+        style={{
+          width: "1px",
+          minWidth: "100%",
+          height: "100vh",
+          border: "none",
+        }}
+        license="GPLv3"
       />
     </FrontPageLayout>
   );
