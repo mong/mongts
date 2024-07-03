b03a62c4ad82aa003f647ab00d53946e28ef3f16
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-07-03 13:56:17 +0200

    Fikset størrelen på den nederste delen av footeren

--- apps/skde/src/components/Footer/index.tsx
+++ apps/skde/src/components/Footer/index.tsx
@@ -67,8 +67,9 @@ export const Footer = ({ page }: FooterProps) => {
           </Stack>
         </Grid>
       </Grid>
-
+      
       <Grid
+        xs={12}
         container
         className="footer"
         style={{ background: "#1A1A1A" }}
