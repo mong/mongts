7cf16719a6c882616df06b238afab10ae6e5331f
stein-kato <steinkato.lindberg@yahoo.no>
2024-07-03 15:03:23 +0200

    Finn enhetsnivå

--- apps/skde/src/components/SubUnits/index.tsx
+++ apps/skde/src/components/SubUnits/index.tsx
@@ -3,12 +3,35 @@ import { ItemBox } from "../HospitalProfileStyles";
 import { Text } from "@visx/text";
 
 type SubUnitsProps = {
-  nestedUnitNames: NestedTreatmentUnitName;
+  nestedUnitNames: NestedTreatmentUnitName[];
   selectedUnit: string;
+};
+
+const getUnitLevel = (
+  RHFs: NestedTreatmentUnitName[],
+  selectedUnit: string,
+) => {
+  let unitLevel: string;
+
+  const RHFNames = RHFs.map((row) => row.rhf);
+  const HFs = RHFs.map((row) => row.hf).flat();
+  const HFNames = HFs.map((row) => row.hf);
+
+  selectedUnit === "Nasjonalt"
+    ? (unitLevel = "Nasjonalt")
+    : RHFNames.includes(selectedUnit)
+      ? (unitLevel = "RHF")
+      : HFNames.includes(selectedUnit)
+        ? (unitLevel = "HF")
+        : (unitLevel = "Hospital");
+
+  return unitLevel;
 };
 
 export const SubUnits = (props: SubUnitsProps) => {
   const { nestedUnitNames, selectedUnit } = props;
+
+  const unitLevel = getUnitLevel(nestedUnitNames, selectedUnit);
 
   return (
     <ItemBox>
@@ -20,6 +43,14 @@ export const SubUnits = (props: SubUnitsProps) => {
         style={{ fontWeight: 700, fontSize: 24 }}
       >
         Utvalgte indikatorer
+      </Text>
+      <Text
+        x={"10%"}
+        width={500}
+        verticalAnchor="start"
+        style={{ fontWeight: 500, fontSize: 18 }}
+      >
+        {unitLevel}
       </Text>
     </ItemBox>
   );
