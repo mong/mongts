056cd605642a88da4d1f3b66d857fc68ae57b4f3
Jo Inge Arnes <jo.inge.arnes@live.no>
2024-08-14 16:42:55 +0200

    redirect from frontpage to skde.no (#2866)

--- apps/skde/pages/_document.tsx
+++ apps/skde/pages/_document.tsx
@@ -1,27 +1,39 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document<{ lang: string }> {
+class MyDocument extends Document<{ lang: string; pathname: string }> {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     const { pathname } = ctx;
     const lang =
       pathname.includes("/en/") || pathname.endsWith("/en") ? "en" : "no";
-    return { ...initialProps, lang };
+    return { ...initialProps, lang, pathname };
   }
 
   render() {
-    const { lang } = this.props;
+    const { lang, pathname } = this.props;
+
     return (
       <Html lang={lang}>
         <Head>
-          <meta
-            name="google-site-verification"
-            content="XnhRhaBwfeuAccfCFysPPpe1f49t46JAfInHBDfK6HE"
-          />
+          {pathname === "/" ? (
+            <meta
+              http-equiv="refresh"
+              content="0; url=https://www.skde.no/"
+            ></meta>
+          ) : (
+            <meta
+              name="google-site-verification"
+              content="XnhRhaBwfeuAccfCFysPPpe1f49t46JAfInHBDfK6HE"
+            />
+          )}
         </Head>
         <body>
-          <Main />
-          <NextScript />
+          {pathname !== "/" && (
+            <>
+              <Main />
+              <NextScript />
+            </>
+          )}
         </body>
       </Html>
     );
