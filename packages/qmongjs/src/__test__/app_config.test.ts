d59f630fb1d8259b8b1876d332132865e5c4b6b5
arnfinn <arnfinn.steindal@gmail.com>
2024-08-16 13:34:04 +0200

    test

--- packages/qmongjs/src/__test__/app_config.test.ts
+++ packages/qmongjs/src/__test__/app_config.test.ts
@@ -1,6 +1,18 @@
-import { minYear, maxYear } from "../app_config";
+import { minYear, maxYear, mainHospitals } from "../app_config";
 import { test, expect } from "vitest";
 
 test("Number of selectable years are 5", () => {
   expect(maxYear - minYear + 1).toBe(5);
+});
+
+test("mainHospitals is an array", () => {
+  expect(Array.isArray(mainHospitals)).toBe(true);
+});
+
+test("mainHospitals is not empty", () => {
+  expect(mainHospitals.length).toBeGreaterThan(0);
+});
+
+test("mainHospitals does not contain rubish", () => {
+  expect(mainHospitals.includes("Porsgrunn")).toBe(false);
 });
