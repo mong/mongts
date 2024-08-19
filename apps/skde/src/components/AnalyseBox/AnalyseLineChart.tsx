602ea32586dc7629dffb6d4246d82e2cb657412c
Mattias Ugelvik <mattias.ugelvik@helse-nord.no>
2024-08-19 12:59:06 +0200

    Simplifiserte koden ved å bruke default tooltip for linjediagram

--- apps/skde/src/components/AnalyseBox/AnalyseLineChart.tsx
+++ apps/skde/src/components/AnalyseBox/AnalyseLineChart.tsx
@@ -3,58 +3,45 @@ import { LineChart } from "@mui/x-charts/LineChart";
 import { names } from "./nameMapping";
 import React from "react";
 
-import { Box } from "@mui/material";
-
-
-const CustomTooltip = (props: any) => {
-  const {itemData, series, sx, classes } = props;
-  console.log(props);
-  return (
-  <Box sx={{ background: "white", padding: "10px", border: "dashed blue"}}>
-    {series.label}
-  </Box>
-  );
-}
-
 type AnalyseLineChartProps = {
   analyse: AnalyseData;
   years: number[];
   level: "region" | "sykehus";
-}
+};
 export const useWindowWidth = () => {
   const [width, setWidth] = React.useState(window.innerWidth);
 
   React.useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
   return width;
 };
 
-
-export const AnalyseLineChart = ({ analyse, years, level }: AnalyseLineChartProps) => {
-
+export const AnalyseLineChart = ({
+  analyse,
+  years,
+  level,
+}: AnalyseLineChartProps) => {
   const windowWidth = useWindowWidth();
 
   const dataset = React.useMemo(() => {
     const dataset = [];
-    for (let year of [...years].reverse()) {
-      let datapoint = {
+    for (const year of [...years].reverse()) {
+      const datapoint = {
         year: year,
-        sum: 0
+        sum: 0,
       };
-      for (let area of Object.keys(analyse.data[level])) {
-        if (datapoint[area] === undefined)
-          datapoint[area] = 0;
+      for (const area of Object.keys(analyse.data[level])) {
+        if (datapoint[area] === undefined) datapoint[area] = 0;
         for (let i = 0; i < analyse.variables.length; i++) {
           datapoint[area] += analyse.data[level][area][year][i];
         }
       }
       dataset.push(datapoint);
     }
-    console.log("REcalculARING");
     return dataset;
   }, [analyse, years, level]);
 
@@ -62,38 +49,40 @@ export const AnalyseLineChart = ({ analyse, years, level }: AnalyseLineChartProp
 
   return (
     <LineChart
-      margin={{ left: 70, top:
-        level === "region"
-        ? 80 
-        : 145 + 60 * (1-smallFactor) }}
+      margin={{
+        left: 70,
+        top: level === "region" ? 80 : 145 + 60 * (1 - smallFactor),
+      }}
       dataset={dataset}
-      xAxis={[{
-        scaleType: 'point',
-        dataKey: 'year',
-        valueFormatter: (y) => y.toString(),
-        tickPlacement: "middle"
-      }]}
+      xAxis={[
+        {
+          scaleType: "point",
+          dataKey: "year",
+          valueFormatter: (y) => y.toString(),
+          tickPlacement: "middle",
+        },
+      ]}
       series={Object.keys(analyse.data[level]).map((area) => ({
         dataKey: area,
         id: area,
         curve: "linear",
         showMark: false,
-        label: names[level][area]
+        label: names[level][area],
       }))}
       onLineClick={(a, b) => console.log("Info: ", a, b)}
       tooltip={{ trigger: "axis" }}
       axisHighlight={{
-        x: 'none'
-      }}      
+        x: "none",
+      }}
       slotProps={{
         legend: {
-          direction: 'row',
-          position: { vertical: 'top', horizontal: 'middle' },
+          direction: "row",
+          position: { vertical: "top", horizontal: "middle" },
           padding: {
             top: 20,
             left: 10,
             bottom: 20,
-            right: 20
+            right: 20,
           },
           itemMarkHeight: 5,
           itemMarkWidth: 5 + Math.round(15 * smallFactor),
@@ -102,18 +91,11 @@ export const AnalyseLineChart = ({ analyse, years, level }: AnalyseLineChartProp
           },
         },
       }}
-      slots={{
-        axisContent: CustomTooltip,
-        lineHighlight: () => (<></>)
-      }}
       sx={{
-        '& .MuiLineElement-root:hover': {
-          strokeWidth: "5px"  
+        "& .MuiLineElement-root:hover": {
+          strokeWidth: "5px",
         },
-        '& .MuiLineElement-series-8888': {
-        }
       }}
-
     />
   );
 };
