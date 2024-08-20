8fcf7fd4f7024ec59045aef13d6af3e2c99ac232
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2024-08-20 14:50:24 +0200

    Kart i infoboks i sykehusprofil (#2899)

--- apps/skde/pages/sykehusprofil/index.tsx
+++ apps/skde/pages/sykehusprofil/index.tsx
@@ -55,6 +55,10 @@ import { ExpandableItemBox } from "../../src/components/ExpandableItemBox";
 import { NestedTreatmentUnitName, URLs } from "types";
 import { ArrowLink } from "qmongjs";
 import { useRouter } from "next/router";
+import { FetchMap } from "../../src/helpers/hooks";
+import { mapColors, abacusColors } from "../../src/charts/colors";
+import { geoMercator, geoPath } from "d3-geo";
+import { mapUnitName2BohfNames } from "../../src/helpers/functions/unitName2BohfMap";
 
 const getUnitFullName = (
   nestedUnitNames: NestedTreatmentUnitName[],
@@ -87,6 +91,8 @@ const getUnitFullName = (
 
 export const Skde = (): JSX.Element => {
   const [expanded, setExpanded] = useState(false);
+
+  const [objectIDList, setObjectIDList] = useState([]);
 
   const treatmentUnitsKey = "selected_treatment_units";
 
@@ -155,6 +161,8 @@ export const Skde = (): JSX.Element => {
         return row.shortName === newUnit[0];
       });
     }
+
+    setObjectIDList(mapUnitName2BohfNames(treatmentUnits.treedata, newUnit[0]));
 
     if (unitUrl && unitUrl[0]) {
       setUnitUrl(unitUrl[0].url);
@@ -305,6 +313,39 @@ export const Skde = (): JSX.Element => {
     );
   };
 
+  // Copy-paste from helseatlas
+  const mapData = FetchMap("/helseatlas/kart/kronikere.geojson").data;
+
+  const mapHeight = 1000;
+  const mapWidth = 1000;
+  const initCenter = geoPath().centroid(mapData);
+  const initOffset: [number, number] = [
+    mapWidth / 2,
+    mapHeight / 2 - mapHeight * 0.11,
+  ];
+  const initScale = 150;
+  const initialProjection = geoMercator()
+    .scale(initScale)
+    .center(initCenter)
+    .translate(initOffset);
+  const initPath = geoPath().projection(initialProjection);
+
+  const bounds = initPath.bounds(mapData);
+  const hscale = (initScale * mapWidth) / (bounds[1][0] - bounds[0][0]);
+  const vscale = (initScale * mapHeight) / (bounds[1][1] - bounds[0][1]);
+  const scale = hscale < vscale ? 0.98 * hscale : 0.98 * vscale;
+  const offset: [number, number] = [
+    mapWidth - (bounds[0][0] + bounds[1][0]) / 2,
+    mapHeight - (bounds[0][1] + bounds[1][1]) / 2,
+  ];
+
+  const projection = geoMercator()
+    .scale(scale)
+    .center(initCenter)
+    .translate(offset);
+
+  const pathGenerator = geoPath().projection(projection);
+
   return (
     <ThemeProvider theme={skdeTheme}>
       <PageWrapper>
@@ -363,7 +404,7 @@ export const Skde = (): JSX.Element => {
         <Box marginTop={2} className="hospital-profile-box">
           <Grid container spacing={2}>
             <Grid xs={12}>
-              <ItemBox height={440} sx={{ overflow: "auto" }}>
+              <ItemBox height={550} sx={{ overflow: "auto" }}>
                 <Grid container>
                   <Grid
                     xs={12}
@@ -420,10 +461,38 @@ export const Skde = (): JSX.Element => {
                           )}
                       </Typography>
 
-                      <div style={{ marginLeft: 20 }}>
-                        <Typography variant="body1">
-                          Her skal det stå noe om enheten. <br />
-                        </Typography>
+                      <div
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          margin: "auto",
+                        }}
+                      >
+                        <svg
+                          width={"400px"}
+                          height={"100%"}
+                          viewBox={`0 0 ${mapWidth} ${mapHeight}`}
+                          style={{ backgroundColor: "none" }}
+                        >
+                          {mapData &&
+                            mapData.features.map((d, i) => {
+                              return (
+                                <path
+                                  key={`map-feature-${i}`}
+                                  d={pathGenerator(d.geometry)}
+                                  fill={
+                                    objectIDList &&
+                                    objectIDList.includes(d.properties.BoHF_num)
+                                      ? abacusColors[2]
+                                      : mapColors[1]
+                                  }
+                                  stroke={"black"}
+                                  strokeWidth={0.4}
+                                  className={i + ""}
+                                />
+                              );
+                            })}
+                        </svg>
                       </div>
 
                       <div style={{ marginTop: "auto" }}>
@@ -442,7 +511,7 @@ export const Skde = (): JSX.Element => {
 
                   <Grid xs={6} sm={6} lg={4} xl={4} xxl={4}>
                     <ItemBox
-                      height={440}
+                      height={450}
                       sx={{ overflow: "auto", marginRight: 2 }}
                     >
                       <Typography variant="h5" style={titleStyle}>
