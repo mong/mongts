c676c387dd4854f2aaaa7b32ec7a6203f5693db9
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2024-09-02 10:11:25 +0200

    Tilbake-knapp (#3024)

--- apps/skde/src/components/SubUnits/index.tsx
+++ apps/skde/src/components/SubUnits/index.tsx
@@ -1,7 +1,43 @@
+import { ReactElement } from "react";
 import { NestedTreatmentUnitName } from "types";
 import { Button, List, ListItem, Stack, Typography, Box } from "@mui/material";
-import { LocalHospital } from "@mui/icons-material";
+import { LocalHospital, Undo } from "@mui/icons-material";
 import { skdeTheme } from "qmongjs";
+
+const getParentUnit = (
+  nestedUnitNames: NestedTreatmentUnitName[],
+  unitShortName: string,
+) => {
+  if (!nestedUnitNames || !unitShortName) {
+    return null;
+  }
+
+  if (unitShortName === "Nasjonalt") {
+    return null;
+  }
+
+  // Check if unit is a RHF
+  const isRHF = nestedUnitNames.map((row) => row.rhf).includes(unitShortName);
+
+  if (isRHF) {
+    return "Nasjonalt";
+  }
+
+  // Check if unit is a HF
+  const HFs = nestedUnitNames.map((row) => row.hf).flat();
+  const isHF = HFs.map((row) => row.hf).includes(unitShortName);
+
+  if (isHF) {
+    return nestedUnitNames.filter((row) => {
+      return row.hf.map((hf) => hf.hf).includes(unitShortName);
+    })[0].rhf;
+  }
+
+  // Check if unit is a hospital
+  return HFs.filter((row) => {
+    return row.hospital.includes(unitShortName);
+  })[0].hf;
+};
 
 type SubUnitsProps = {
   RHFs: NestedTreatmentUnitName[];
@@ -60,57 +96,86 @@ export const SubUnits = (props: SubUnitsProps) => {
 
   const buttonVariant = "outlined";
 
-  return unitLevel === "Nasjonalt" ? (
-    <List>
-      {RHFs.sort((a, b) => a.rhf_sort - b.rhf_sort)
-        .map((row) => row.rhf)
-        .map((rhf) => {
-          return (
-            <ListItem key={"subunit-link-" + rhf}>
-              <UnitButton unitName={rhf} buttonVariant={buttonVariant} />
-            </ListItem>
-          );
-        })}
-    </List>
-  ) : unitLevel === "RHF" ? (
-    <List>
-      {RHFs.filter((row) => row.rhf === selectedUnit)[0]
-        .hf.filter(
-          (row) =>
-            !row.hf.includes("Private") &&
-            !row.hf.includes("Avtalespesialister"),
-        )
-        .map((row) => {
-          return (
-            <ListItem key={"subunit-link-" + row.hf}>
-              <UnitButton unitName={row.hf} buttonVariant={buttonVariant} />
-            </ListItem>
-          );
-        })}
-    </List>
-  ) : unitLevel === "HF" ? (
-    <List>
-      {HFs.filter((row) => row.hf === selectedUnit)[0].hospital.map(
-        (hospital) => {
-          return (
-            <ListItem key={"subunit-link-" + hospital}>
-              <UnitButton unitName={hospital} buttonVariant={buttonVariant} />
-            </ListItem>
-          );
-        },
-      )}
-    </List>
-  ) : (
-    <Box
-      sx={{
-        backgroundColor: skdeTheme.palette.warning.main,
-        marginLeft: 2,
-        marginTop: 5,
-      }}
-    >
-      <Typography variant="body1">
-        Denne enheten har ingen underliggende enheter.
-      </Typography>
-    </Box>
+  let buttonList: ReactElement;
+
+  const parentUnit = getParentUnit(RHFs, selectedUnit);
+  console.log(parentUnit);
+
+  if (unitLevel === "Nasjonalt") {
+    buttonList = (
+      <List>
+        {RHFs.sort((a, b) => a.rhf_sort - b.rhf_sort)
+          .map((row) => row.rhf)
+          .map((rhf) => {
+            return (
+              <ListItem key={"subunit-link-" + rhf}>
+                <UnitButton unitName={rhf} buttonVariant={buttonVariant} />
+              </ListItem>
+            );
+          })}
+      </List>
+    );
+  } else if (unitLevel === "RHF") {
+    buttonList = (
+      <List>
+        {RHFs.filter((row) => row.rhf === selectedUnit)[0]
+          .hf.filter(
+            (row) =>
+              !row.hf.includes("Private") &&
+              !row.hf.includes("Avtalespesialister"),
+          )
+          .map((row) => {
+            return (
+              <ListItem key={"subunit-link-" + row.hf}>
+                <UnitButton unitName={row.hf} buttonVariant={buttonVariant} />
+              </ListItem>
+            );
+          })}
+      </List>
+    );
+  } else if (unitLevel === "HF") {
+    buttonList = (
+      <List>
+        {HFs.filter((row) => row.hf === selectedUnit)[0].hospital.map(
+          (hospital) => {
+            return (
+              <ListItem key={"subunit-link-" + hospital}>
+                <UnitButton unitName={hospital} buttonVariant={buttonVariant} />
+              </ListItem>
+            );
+          },
+        )}
+      </List>
+    );
+  } else {
+    buttonList = (
+      <List
+        sx={{
+          backgroundColor: skdeTheme.palette.warning.main,
+          marginLeft: 2,
+          marginTop: 5,
+        }}
+      >
+        <Typography variant="body1">
+          Denne enheten har ingen underliggende enheter.
+        </Typography>
+      </List>
+    );
+  }
+
+  return (
+    <>
+      {buttonList}
+      <Box marginLeft={2} marginTop={4}>
+        {parentUnit && (
+          <Button
+            variant="contained"
+            href={"/sykehusprofil/?selected_treatment_units=" + parentUnit}
+          >
+            <Undo /> <Typography variant="button">Opp et nivå</Typography>{" "}
+          </Button>
+        )}
+      </Box>
+    </>
   );
 };
