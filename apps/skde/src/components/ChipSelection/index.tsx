468a075aa3610495a930d6921cec72843597cdf4
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2024-09-04 15:09:26 +0200

    Flyttet hoverbox til qmongjs (#3059)

--- apps/skde/src/components/ChipSelection/index.tsx
+++ apps/skde/src/components/ChipSelection/index.tsx
@@ -3,7 +3,7 @@
 // If the left chip is selected when the state is true, set trueChip=left, and vice versa.
 
 import { Chip, Stack } from "@mui/material";
-import { Hoverbox } from "../Hoverbox";
+import { Hoverbox } from "qmongjs";
 
 type ChipSelectionProps = {
   leftChipLabel: string;
