e0bd500a87b75545772e6f9683076b7f10822c9a
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2024-09-10 16:04:23 +0200

    Advarsel i header hvis bruker er på verify-siden (#3104)
    Miljøvariablen NEXT_PUBLIC_VERIFY må være definert for at advarsel skal dukke opp. Lenken går til tilsvarende side på apps.skde.no, med de valg to har aktivert.
    
    Closes #524

--- apps/skde/src/components/Header/HeaderMiddle.tsx
+++ apps/skde/src/components/Header/HeaderMiddle.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Toolbar, Typography, styled, Container } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { Breakpoint } from "@mui/material";
+import { useRouter } from "next/router";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   paddingTop: theme.spacing(12),
@@ -21,14 +22,33 @@ type HeaderMiddleProps = {
 };
 
 export const HeaderMiddle = (props: HeaderMiddleProps) => {
+  const router = useRouter();
+  const mainUrl = "https://apps.skde.no" + router.asPath;
+
   return (
     <StyledToolbar
-      sx={{ bgcolor: props.bgcolor || "primary.light" }}
+      sx={{
+        bgcolor: process.env.NEXT_PUBLIC_VERIFY
+          ? "error.main"
+          : props.bgcolor || "primary.light",
+      }}
       className="header-middle"
     >
       <Container maxWidth={props.maxWidth} disableGutters={true}>
         <Grid container spacing={2} rowSpacing={6}>
           <Grid size={{ xs: 12 }}>
+            {process.env.NEXT_PUBLIC_VERIFY && (
+              <>
+                <Typography variant="h6">
+                  OBS! Dette er en verifiseringsside for testdata. Hvis du
+                  ønsker å se offisielle tall må du gå til{" "}
+                  <a href={mainUrl} target="_self">
+                    www.skde.no.
+                  </a>
+                </Typography>
+                <br />
+              </>
+            )}
             <Typography variant="h1">{props.headerData.title}</Typography>
           </Grid>
           <Grid size={{ xs: 12 }}>
