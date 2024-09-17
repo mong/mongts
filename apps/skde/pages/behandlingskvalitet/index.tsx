f04bdb720adc654f9ef13f5f4aa21bf4076735ab
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2024-09-17 10:30:51 +0200

    Mer rom til meny på mellomstore skjermer (#3085)
    La inn et ekstra breakpoint mellom xxl og xxxl, og lot meny få fire kolonner mellom xxl og nye xxml. Tre av 12 kolonner ble for trangt.

--- apps/skde/pages/behandlingskvalitet/index.tsx
+++ apps/skde/pages/behandlingskvalitet/index.tsx
@@ -303,7 +303,7 @@ export default function TreatmentQualityPage() {
         />
         <Grid container size={{ xs: 12 }}>
           {useMediaQuery(skdeTheme.breakpoints.up("xxl")) ? ( // Permanent menu on large screens
-            <Grid size={{ xxl: 3, xxxl: 2 }} className="menu-wrapper">
+            <Grid size={{ xxl: 4, xxml: 3, xxxl: 2 }} className="menu-wrapper">
               {queriesReady && (
                 <Box
                   sx={{
@@ -325,7 +325,7 @@ export default function TreatmentQualityPage() {
               )}
             </Grid>
           ) : null}
-          <Grid size={{ xs: 12, xxl: 9, xxxl: 10 }}>
+          <Grid size={{ xs: 12, xxl: 8, xxml: 9, xxxl: 10 }}>
             <Grid container spacing={2}>
               <Grid size={{ xs: 12 }}>
                 {queriesReady &&
