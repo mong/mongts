12dad8b99aafa7a758e2e16e485c342d08693953
Jo Inge Arnes <jo.inge.arnes@live.no>
2024-10-11 13:43:31 +0200

    Flytte valg mellom behandlingsenheter og opptaksområder fra arkfaner til filtermeny (#3276)
    * in-flight changes
    
    * SwitchFilterSection should take helper text as input property
    
    * working on table context selection in filter menu
    
    * The mechanics of the toggle button and table context works now
    
    * made updates to registry specific page
    
    * removing special filter menu for registers
    
    * refactoring, etc.
    
    * adjusting look of toggle buttons, etc.
    
    * removed import of no-longer existing type
    
    * adding safe-guards
    
    * checking potential empty input variables that are needed
    
    * reverting prettier md-files
    
    * revert prettier tsconfig
    
    * prettier
    
    * revert prettier lintstagerc
    
    * removed unused import
    
    * fixed knip complaint
    
    * check for cypress tests
    
    * making typescript happy about document
    
    * added new test id
    
    * fixed cypress test
    
    * prettier
    
    * lab-atlas from develop
    
    ---------
    
    Co-authored-by: jo-inge-arnes <jo.inge.arnes@helse-nord.no>

--- packages/qmongjs/src/index.tsx
+++ packages/qmongjs/src/index.tsx
@@ -31,6 +31,7 @@ export {
   FilterSettingsActionType,
 } from "./components/FilterMenu/FilterSettingsReducer";
 export type { FilterSettingsAction } from "./components/FilterMenu/FilterSettingsReducer";
+export { ToggleButtonFilterSection } from "./components/FilterMenu/ToggleButtonFilterSection";
 export { RadioGroupFilterSection } from "./components/FilterMenu/RadioGroupFilterSection";
 export { SelectedFiltersSection } from "./components/FilterMenu/SelectedFiltersSection";
 export { SwitchFilterSection } from "./components/FilterMenu/SwitchFilterSection";
@@ -40,8 +41,10 @@ export {
   TreatmentQualityFilterMenu,
   levelKey,
   treatmentUnitsKey,
+  tableContextKey,
   yearKey,
   medicalFieldKey,
+  dataQualityKey,
 } from "./components/FilterMenu/TreatmentQualityFilterMenu";
 export type { TreatmentQualityFilterMenuProps } from "./components/FilterMenu/TreatmentQualityFilterMenu";
 export {
