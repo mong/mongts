331a45b219e159b487bdbe1c4dc88dea3b8c3c08
jo-inge-arnes <jo.inge.arnes@helse-nord.no>
2024-10-21 15:36:15 +0200

    fixed isolated pages

--- apps/skde/pages/helseatlas/iframetester/index.tsx
+++ apps/skde/pages/helseatlas/iframetester/index.tsx
@@ -1,11 +1,20 @@
+import { useSearchParams } from "next/navigation";
+
 export default function IframeTester() {
+  const searchParams = useSearchParams();
+  const domainParam = searchParams.get("domain");
+  const domain = domainParam ? domainParam : "https://localhost:3000";
+
   return (
     <>
       <h1>IframeTester</h1>
       <div>
         <iframe
           id="iframe-child-3"
-          src="/helseatlas/kulelinje/?atlas=lab&data=b12_rb1"
+          src={
+            new URL("/helseatlas/kulelinje/?atlas=lab&amp;data=b12_rb1", domain)
+              .href
+          }
           title="iframe-child-3"
           width="500"
           height="200"
@@ -14,7 +23,12 @@ export default function IframeTester() {
       <div>
         <iframe
           id="iframe-child-1"
-          src="/helseatlas/resultatboks/?atlas=lab&data=b12_rb1"
+          src={
+            new URL(
+              "/helseatlas/resultatboks/?atlas=lab&amp;data=b12_rb1",
+              domain,
+            ).href
+          }
           title="iframe-child-1"
           width="500"
           height="600"
@@ -23,8 +37,22 @@ export default function IframeTester() {
       <div>
         <iframe
           id="iframe-child-2"
-          src="/helseatlas/resultatboks/?atlas=lab&data=b12_rb1"
+          src={
+            new URL(
+              "/helseatlas/resultatboks/?atlas=lab&amp;data=b12_rb1",
+              domain,
+            ).href
+          }
           title="iframe-child-2"
+          width="500"
+          height="600"
+        />
+      </div>
+      <div>
+        <iframe
+          id="iframe-child-vg"
+          src="https://vg.no"
+          title="iframe-child-vg"
           width="500"
           height="600"
         />
