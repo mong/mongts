aed1b0a328e6d35e66d4967ce912abc2a4593f2b
Jo Inge Arnes <jo.i.arnes@uit.no>
2024-11-05 10:37:35 +0100

    kommenterer ut cypress-test for helseatlas-forsiden

--- apps/skde/test/cypress/integration/home.cy.tsx
+++ apps/skde/test/cypress/integration/home.cy.tsx
@@ -1,49 +1,51 @@
 /// <reference types="cypress"/>
 
-context("Push some front page buttons", () => {
-  beforeEach(() => {
-    cy.visit("/helseatlas");
-  });
+// The tests are commented out because the new front page is published on FNSP
 
-  it("should visit v2 atlas", () => {
-    cy.get('[data-testid="helseatlas/v2/kronikere"]').click();
-    cy.get('[data-testid="v2atlas"]', { timeout: 60000 }).should("exist");
-    cy.url().should("include", "/v2");
-  });
+// context("Push some front page buttons", () => {
+//   beforeEach(() => {
+//     cy.visit("/helseatlas");
+//   });
 
-  it("should visit v1 atlas", () => {
-    cy.get('[data-testid="helseatlas/v2/dagkir"]').click();
-    cy.get('[data-testid="v2atlas"]', { timeout: 60000 }).should("exist");
-    cy.url().should("include", "/v2");
-  });
+//   it("should visit v2 atlas", () => {
+//     cy.get('[data-testid="helseatlas/v2/kronikere"]').click();
+//     cy.get('[data-testid="v2atlas"]', { timeout: 60000 }).should("exist");
+//     cy.url().should("include", "/v2");
+//   });
 
-  it("should push english button", () => {
-    cy.get('[data-testid="buttonEng"]').click(); // Push english button
-    cy.get('[data-testid="helseatlas/en/v2/kvalitet"]', {
-      timeout: 60000,
-    }).should("exist"); // Wait for english page to load
-    cy.url().should("include", "/en/");
-    cy.get("h1").contains("Equitable health services");
-    cy.get('[data-testid="menuButton"]').click(); // Enter english menu
-    cy.get('[data-testid="mainMenu"]').should("exist"); // Menu exist
-    cy.get('[data-testid="menuAtlasLink1"]').click(); // Enter an english atlas
-    cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu gone after click
-    cy.url({ timeout: 60000 }).should("include", "/en/v2/"); // English atlas is entered in english menu
-  });
+//   it("should visit v1 atlas", () => {
+//     cy.get('[data-testid="helseatlas/v2/dagkir"]').click();
+//     cy.get('[data-testid="v2atlas"]', { timeout: 60000 }).should("exist");
+//     cy.url().should("include", "/v2");
+//   });
 
-  it("should push menu button", () => {
-    cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu does not exist
-    cy.get('[data-testid="menuButton"]').click();
-    cy.get('[data-testid="mainMenu"]').should("exist"); // Menu exist
+//   it("should push english button", () => {
+//     cy.get('[data-testid="buttonEng"]').click(); // Push english button
+//     cy.get('[data-testid="helseatlas/en/v2/kvalitet"]', {
+//       timeout: 60000,
+//     }).should("exist"); // Wait for english page to load
+//     cy.url().should("include", "/en/");
+//     cy.get("h1").contains("Equitable health services");
+//     cy.get('[data-testid="menuButton"]').click(); // Enter english menu
+//     cy.get('[data-testid="mainMenu"]').should("exist"); // Menu exist
+//     cy.get('[data-testid="menuAtlasLink1"]').click(); // Enter an english atlas
+//     cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu gone after click
+//     cy.url({ timeout: 60000 }).should("include", "/en/v2/"); // English atlas is entered in english menu
+//   });
 
-    cy.get('[data-testid="menuAtlasLink1"]').click();
-    cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu gone after click
+//   it("should push menu button", () => {
+//     cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu does not exist
+//     cy.get('[data-testid="menuButton"]').click();
+//     cy.get('[data-testid="mainMenu"]').should("exist"); // Menu exist
 
-    cy.get('[data-testid="menuButton"]').click(); // Push menu button again
-    cy.get('[data-testid="mainMenu"]').should("exist"); // Menu exist
-    cy.get('[data-testid="menuButton"]').type("{esc}"); // click Esc to exit menu
-    cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu gone after click
-  });
-});
+//     cy.get('[data-testid="menuAtlasLink1"]').click();
+//     cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu gone after click
+
+//     cy.get('[data-testid="menuButton"]').click(); // Push menu button again
+//     cy.get('[data-testid="mainMenu"]').should("exist"); // Menu exist
+//     cy.get('[data-testid="menuButton"]').type("{esc}"); // click Esc to exit menu
+//     cy.get('[data-testid="mainMenu"]').should("not.exist"); // Menu gone after click
+//   });
+// });
 
 export {};
