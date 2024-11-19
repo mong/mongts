2faa5d9c4c9340d7163ec6c65d6a6bd2de1756ab
Jo Inge Arnes <jo.i.arnes@uit.no>
2024-11-19 15:39:28 +0100

    trying to improve query performance

--- apps/api/src/helpers/functions/createOptsTu.ts
+++ apps/api/src/helpers/functions/createOptsTu.ts
@@ -1,13 +1,13 @@
 import { OptsTu, TuName } from "types";
 
 export const createOptsTu = (
-  distinctUnitNames: { unit_name: string }[] | null,
+  distinctUnitNames: { unit_name: string }[],
   allUnitNames: TuName[],
 ): OptsTu[] => {
-  const dunArray = distinctUnitNames?.map((names) => names.unit_name);
+  const dunArray = distinctUnitNames.map((names) => names.unit_name);
 
   const opts_hosp = allUnitNames
-    .filter((unitName) => dunArray?.includes(unitName.hospital) ?? true)
+    .filter((unitName) => dunArray.includes(unitName.hospital))
     .map((unitName) => {
       return {
         label: unitName.hospital,
@@ -17,7 +17,7 @@ export const createOptsTu = (
   const opts_hf = Array.from(
     new Set(
       allUnitNames
-        .filter((unitName) => dunArray?.includes(unitName.hf) ?? true)
+        .filter((unitName) => dunArray.includes(unitName.hf))
         .map((unitName) => unitName.hf),
     ),
   ).map((unitName) => {
@@ -29,7 +29,7 @@ export const createOptsTu = (
   const opts_rhf = Array.from(
     new Set(
       allUnitNames
-        .filter((unitName) => dunArray?.includes(unitName.rhf) ?? true)
+        .filter((unitName) => dunArray.includes(unitName.rhf))
         .map((unitName) => unitName.rhf),
     ),
   ).map((unitName) => {
