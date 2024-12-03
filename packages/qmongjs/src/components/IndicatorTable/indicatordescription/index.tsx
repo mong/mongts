3ecb436d10d7396d155bf18dd29dd4c3a96e0695
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2024-12-03 10:16:56 +0100

    Sett større/mindre eller lik på ikke andel (#3570)

--- packages/qmongjs/src/components/IndicatorTable/indicatordescription/index.tsx
+++ packages/qmongjs/src/components/IndicatorTable/indicatordescription/index.tsx
@@ -19,13 +19,36 @@ export const IndicatorDescription = (props: DescriptionProps) => {
   const short_description = description.short_description;
   const level_direction = description.level_direction;
   const level_green = description.level_green;
+  const indType = description.type;
+  const maxVal = description.max_value;
+  const minVal = description.min_value;
 
   let level_sign = "";
 
-  if (level_green != null) {
+  // Case: ratio
+  if (indType === "andel" && level_green != null) {
     if (level_direction === 1 && level_green < 1) {
       level_sign = "≥";
     } else if (level_direction === 0 && level_green > 0) {
+      level_sign = "≤";
+    }
+  }
+  // Case: not ratio
+  else if (indType !== "andel" && level_green != null) {
+    // Case: max value set
+    if (level_direction === 1 && maxVal !== null && level_green < maxVal) {
+      level_sign = "≥";
+    }
+    // Case: max value not set
+    else if (level_direction === 1 && maxVal === null) {
+      level_sign = "≥";
+    }
+    // Case: min value set
+    else if (level_direction === 0 && minVal !== null && level_green > minVal) {
+      level_sign = "≤";
+    }
+    // Case: min value not set
+    else if (level_direction === 0 && minVal === null) {
       level_sign = "≤";
     }
   }
