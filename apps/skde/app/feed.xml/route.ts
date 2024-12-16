976fbd51e219fc106482b15c5c562900d52d5d50
stein-kato <steinkato.lindberg@yahoo.no>
2024-12-16 15:14:59 +0100

    Finpuss

--- apps/skde/app/feed.xml/route.ts
+++ apps/skde/app/feed.xml/route.ts
@@ -1,10 +1,11 @@
 import RSS from "rss";
-import { Description } from "types";
-
+import { Description, RegisterName } from "types";
 
 export async function GET() {
-
-  const siteUrl = "http://localhost:3000";
+  const siteUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://apps.skde.no"
+      : "http://localhost:3000";
 
   const feedOptions = {
     title: "RSS Behandlingskvalitet",
@@ -14,28 +15,43 @@ export async function GET() {
     ttl: 60,
   };
 
-  const dataUrls = ["https://prod-mong-api.skde.org/data/hjerneslag/descriptions", "https://prod-mong-api.skde.org/data/hjerteinfarkt/descriptions", "https://prod-mong-api.skde.org/data/ms/descriptions"]
+  const namesUrl = "https://prod-mong-api.skde.org/info/names";
+
+  const namesQuery = await fetch(namesUrl);
+  const namesResponse = await namesQuery.json();
+  const names = namesResponse.map((row: RegisterName) => row.rname);
+
+  const dataUrls = names.map(
+    (name: string) =>
+      "https://prod-mong-api.skde.org/data/" + name + "/descriptions",
+  );
 
   const feed = new RSS(feedOptions);
 
   async function getData(url: string) {
     const response = await fetch(url);
     const data = await response.json();
-    data.map((row: Description) => feed.item({
-      title: row.title,
-      description: row.short_description + row.long_description,
-      url: "www.vg.no",
-      date: "16.12.2024"
-    }));
+
+    data.map((row: Description) =>
+      feed.item({
+        title: row.title,
+        description: row.short_description + row.long_description,
+        url:
+          siteUrl +
+          "/behandlingskvalitet/" +
+          row.rname +
+          "/?selected_row=" +
+          row.id,
+        date: new Date(),
+      }),
+    );
   }
-  
-  await Promise.all(
-    dataUrls.map(getData)
-  )
+
+  await Promise.all(dataUrls.map(getData));
 
   return new Response(feed.xml({ indent: true }), {
     headers: {
-        'Content-Type': 'application/xml; charset=utf-8',
+      "Content-Type": "application/xml; charset=utf-8",
     },
   });
 }
