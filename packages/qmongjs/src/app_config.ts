3cab2288d9ff506ecf2ffc7f3e761257f6d92686
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2024-12-19 14:43:02 +0100

    Filtrer ut lav dekningsgrad i sykehusprofil (#3575)
    Co-authored-by: Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>

--- packages/qmongjs/src/app_config.ts
+++ packages/qmongjs/src/app_config.ts
@@ -45,6 +45,9 @@ export const app_text: appTextTypes = {
 export const maxYear = 2024;
 export const minYear = maxYear - 4;
 export const defaultYear = 2023;
+
+export const minDG = 0.6;
+
 export const mainQueryParamsConfig = {
   selected_row: withDefault(StringParam, undefined),
   indicator: withDefault(StringParam, undefined),
