081ef635ac2768b776a9124ac73f68977556fd05
stein-kato <steinkato.lindberg@yahoo.no>
2025-02-25 09:36:57 +0100

    Styling

--- apps/skde/pages/sykehusprofil/index.tsx
+++ apps/skde/pages/sykehusprofil/index.tsx
@@ -222,6 +222,7 @@ export const Skde = (): JSX.Element => {
                 ) : (
                   <SelectedIndicatorTable
                     unitName={unitName}
+                    titlePadding={titlePadding}
                   />
                 )}
               </Grid>
--- apps/skde/src/components/HospitalProfile/SelectedIndicatorTable/index.tsx
+++ apps/skde/src/components/HospitalProfile/SelectedIndicatorTable/index.tsx
@@ -1,18 +1,26 @@
 import { ItemBox } from "../HospitalProfileStyles";
 import { indicatorsPerHospital } from "./indicators";
+import { Typography, Box } from "@mui/material";
 
 export type SelectedIndicatorTableProps = {
-  unitName: string
-}
+  unitName: string;
+  titlePadding: number;
+};
 
 export const SelectedIndicatorTable = (props: SelectedIndicatorTableProps) => {
-  const {unitName} = props;
+  const { unitName, titlePadding } = props;
 
-  const selectedIndicators = indicatorsPerHospital.filter((row) => row.unit === unitName)
-console.log(selectedIndicators[0])
-  return(
+  const selectedIndicators = indicatorsPerHospital.filter(
+    (row) => row.unit === unitName,
+  );
+  console.log(selectedIndicators[0]);
+  return (
     <ItemBox height={400}>
-
+      <Box padding={titlePadding}>
+        <Typography variant="h5">
+          <b>Utvalgte indikatorer</b>
+        </Typography>
+      </Box>
     </ItemBox>
-  )
-}
+  );
+};
