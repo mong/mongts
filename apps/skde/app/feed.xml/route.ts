9fe093f3c479d2715f531398842c66a96d2d9147
jo-inge-arnes <jo.inge.arnes@helse-nord.no>
2025-03-12 13:19:23 +0100

    trigger nytt bygg

--- apps/skde/app/feed.xml/route.ts
+++ apps/skde/app/feed.xml/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  return new Response("RSS feed is temporarly disabled", {
+  return new Response("RSS feed is disabled for now", {
     headers: {
       "Content-Type": "text/plain",
     },
