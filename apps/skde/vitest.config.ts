ab4d939943ffa8ab1d63027e0617d3a96b18edb7
stein-kato <steinkato.lindberg@yahoo.no>
2025-03-31 15:03:38 +0200

    Fikset tester

--- apps/skde/vitest.config.ts
+++ apps/skde/vitest.config.ts
@@ -1,14 +1,13 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vitest/config";
+import { ViteUserConfig, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() as ViteUserConfig["plugins"]],
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: "src/setupTests.ts",
   },
 });
