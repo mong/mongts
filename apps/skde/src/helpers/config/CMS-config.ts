b4ca8a8521c960affa03f7687724388042c52131
Uglemat <mattias.ugelvik@helse-nord.no>
2025-04-07 16:01:17 +0200

    Lastet inn CMS med next/script (#3929)
    Vi har hatt mye problemer med å holde CMS-en oppdatert pga ymse
    konflikter med versjoner av pakker og sånn. Etter å ha eksperimentert en
    del med decap i et annet repo har jeg kommet fram til at disse
    problemene kan løses hvis vi gjør CMS 100% client-basert, og laster inn
    CMS-en via <script>-tags istedenfor å ha en lokal versjon.
    `decap-cms-app` og `decap-cms-core` er fjernet (sammen med et svært
    stort antall dependencies), men `decap-server` er beholdt (og den funker
    som den skal). Når vi gjør det på denne måten så tror jeg det vil være
    lettere å holde CMS-en oppdatert, og jeg tror ikke det vil bli noen
    konflikt rundt React 19 også videre.
    
    Foreløpig har jeg testet det lokalt; hvis jeg kjører `yarn run cms` kan
    jeg bruke CMS-en, og den endrer filer lokalt. Hvis jeg ikke kjører `yarn
    run cms` kan jeg logge inn i cms-en med github, men jeg kan ikke
    publiserer grunnet denne feilmeldingen:
    
    > API_ERROR: Although you appear to have the correct authorization
    credentials, the `mong` organization has enabled OAuth App access
    restrictions, meaning that data access to third-parties is limited. For
    more information on these restrictions, including how to enable this
    app, visit
    https://docs.github.com/articles/restricting-access-to-your-organization-s-data/
    
    Jeg er usikker på om denne feilmeldingen vil forsvinne når CMS-en ligger
    ute på skde.no, så dette må testes.
    
    Jeg har fjernet en linje fra configurasjonen som så ut til å føre til
    problemer lokalt, nemlig denne:
    
    > base_url: process.env.NEXT_PUBLIC_API_HOST ??
    "https://prod-api.skde.org",
    
    Jeg forstår ikke helt hva den er for, og om vi trenger det.
    ([dokumentasjonen er her](https://decapcms.org/docs/backends-overview/))

--- apps/skde/src/helpers/config/CMS-config.ts
+++ apps/skde/src/helpers/config/CMS-config.ts
@@ -1,12 +1,4 @@
-import { CmsCollection, CmsConfig, CmsField } from "decap-cms-core";
-
-const filename: CmsField = {
-  label: "Filnavn",
-  name: "filename",
-  widget: "string",
-};
-
-const atlas = (lang: "no" | "en"): CmsCollection => {
+const atlas = (lang: "no" | "en") => {
   return {
     label: lang === "no" ? "Atlas" : "Engelske atlas",
     name: lang === "no" ? "atlas" : "atlas_eng",
@@ -32,7 +24,7 @@ const atlas = (lang: "no" | "en"): CmsCollection => {
       preview: false,
     },
     fields: [
-      filename,
+      { label: "Filnavn", name: "filename", widget: "string" },
       {
         label: "Publisert på forsiden",
         name: "publisert",
@@ -209,7 +201,7 @@ const atlas = (lang: "no" | "en"): CmsCollection => {
   };
 };
 
-const staticPages = (lang: "no" | "en"): CmsCollection => {
+const staticPages = (lang: "no" | "en") => {
   return {
     label: lang === "no" ? "Statiske sider" : "Statiske engelske sider",
     name: lang === "no" ? "statiske_sider" : "statiske_sider_en",
@@ -219,7 +211,7 @@ const staticPages = (lang: "no" | "en"): CmsCollection => {
         : "apps/skde/_posts/helseatlas/en/static",
     create: true,
     fields: [
-      filename,
+      { label: "Filnavn", name: "filename", widget: "string" },
       {
         label: "Tittel",
         name: "title",
@@ -241,18 +233,18 @@ const staticPages = (lang: "no" | "en"): CmsCollection => {
   };
 };
 
-export const config: CmsConfig = {
+export const config = {
   local_backend: true,
   backend: {
     name: "github",
     repo: "mong/mongts",
     branch: "main",
-    base_url: process.env.NEXT_PUBLIC_API_HOST ?? "https://prod-api.skde.org",
   },
   logo_url: "https://apps.skde.no/helseatlas/img/logos/helseatlas.svg",
   media_folder: "/apps/skde/public/helseatlas/img",
   public_folder: "/helseatlas/img",
   site_url: "https://apps.skde.no/helseatlas/",
+  base_url: process.env.NEXT_PUBLIC_API_HOST ?? "https://prod-api.skde.org",
   locale: "nb_no",
   collections: [atlas("no"), staticPages("no"), atlas("en"), staticPages("en")],
 };
