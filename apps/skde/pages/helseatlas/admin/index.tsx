00a6cc97554314ab47dad71baa06067a1e92b38f
Uglemat <mattias.ugelvik@helse-nord.no>
2025-04-07 16:01:17 +0200

    Lastet inn CMS med next/script (#3929)
    Vi har hatt mye problemer med å holde CMS-en oppdatert pga ymse
    konflikter med versjoner av pakker og sånn. Etter å ha eksperimentert en
    del med decap i et annet repo har jeg kommet fram til at disse
    problemene kan løses hvis vi gjør CMS 100% client-basert, og laster inn
    CMS-en via <script>-tags istedenfor å ha en lokal versjon.
    `decap-cms-app` og `decap-cms-core` er fjernet (sammen med et svært
    stort antall dependencies), men `decap-server` er beholdt (og den funker
    som den skal). Når vi gjør det på denne måten så tror jeg det vil være
    lettere å holde CMS-en oppdatert, og jeg tror ikke det vil bli noen
    konflikt rundt React 19 også videre.
    
    Foreløpig har jeg testet det lokalt; hvis jeg kjører `yarn run cms` kan
    jeg bruke CMS-en, og den endrer filer lokalt. Hvis jeg ikke kjører `yarn
    run cms` kan jeg logge inn i cms-en med github, men jeg kan ikke
    publiserer grunnet denne feilmeldingen:
    
    > API_ERROR: Although you appear to have the correct authorization
    credentials, the `mong` organization has enabled OAuth App access
    restrictions, meaning that data access to third-parties is limited. For
    more information on these restrictions, including how to enable this
    app, visit
    https://docs.github.com/articles/restricting-access-to-your-organization-s-data/
    
    Jeg er usikker på om denne feilmeldingen vil forsvinne når CMS-en ligger
    ute på skde.no, så dette må testes.
    
    Jeg har fjernet en linje fra configurasjonen som så ut til å føre til
    problemer lokalt, nemlig denne:
    
    > base_url: process.env.NEXT_PUBLIC_API_HOST ??
    "https://prod-api.skde.org",
    
    Jeg forstår ikke helt hva den er for, og om vi trenger det.
    ([dokumentasjonen er her](https://decapcms.org/docs/backends-overview/))

--- apps/skde/pages/helseatlas/admin/index.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const InitializeCMS = dynamic(() => import("../../../src/components/CMS"), {
-  ssr: false,
-  loading: () => <p>Loading Admin...</p>,
-});
-
-export default InitializeCMS;
