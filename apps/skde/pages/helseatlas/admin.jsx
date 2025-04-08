f65c7bd6d327685d65a2fcb6204474f500cdfa23
Uglemat <mattias.ugelvik@helse-nord.no>
2025-04-08 14:33:30 +0200

    Fjernet <script> for identify.netlify.com (#3931)
    Det funket ikke å logge på i develop, og det virker som det henger
    sammen med et av scriptene som ble inkludert. I og med at detaljene for
    github-autentisering ligger i configurasjonen tror jeg ikke dette
    scriptet er nødvendig.

--- apps/skde/pages/helseatlas/admin.jsx
+++ apps/skde/pages/helseatlas/admin.jsx
@@ -12,7 +12,6 @@ export function CMS() {
 
   return (
     <>
-      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       <Script
         src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"
         onLoad={() => {
