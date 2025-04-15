df499c27cefdd46d9ccf78ee9dcbbf223de00ea4
Jo Inge Arnes <jo.i.arnes@uit.no>
2025-04-15 12:23:11 +0200

    cachetest

--- apps/skde/pages/behandlingskvalitet/index.tsx
+++ apps/skde/pages/behandlingskvalitet/index.tsx
@@ -271,7 +271,7 @@ export default function TreatmentQualityPage() {
       <CssBaseline />
       <PageWrapper>
         <LayoutHead
-          title="Behandlingskvalitet (cachetest)"
+          title="Behandlingskvalitet - cachetest"
           content="This page shows the quality indicators from national health registries in the Norwegian specialist healthcare service."
           href="/favicon.ico"
         />
