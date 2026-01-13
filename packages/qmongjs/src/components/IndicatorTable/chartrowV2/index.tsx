1ef839e301da89687885499809ce2ec5e4057334
stein-kato <steinkato.lindberg@yahoo.no>
2026-01-13 12:01:38 +0100

    Flyttet søylediagram og dataformatering

--- packages/qmongjs/src/components/IndicatorTable/chartrowV2/index.tsx
+++ packages/qmongjs/src/components/IndicatorTable/chartrowV2/index.tsx
@@ -1,4 +1,5 @@
-import { DataPoint, IndicatorData } from "types";
+import { useState } from "react";
+import { IndicatorData } from "types";
 import {
   Select,
   FormControl,
@@ -7,20 +8,11 @@ import {
   MenuItem,
   Box,
 } from "@mui/material";
-import { useState } from "react";
-import { BarPlot } from "@mui/x-charts";
-import {
-  ChartDataProvider,
-  ChartsSurface,
-  ChartsTooltip,
-  ChartsXAxis,
-  ChartsYAxis,
-  LineSeriesType,
-} from "@mui/x-charts";
 import { getLastCompleteYear } from "../../../helpers/functions";
 import { customFormat } from "../../../helpers/functions";
 import { MuiLineChart } from "../../Charts/MuiLineChart";
-import { BarBackground } from "../../Charts/MuiBarChart/BarBackground";
+import { MuiBarChart } from "../../Charts/MuiBarChart";
+import { formatData } from "./formatData";
 
 type chartRowV2Props = {
   data: IndicatorData;
@@ -28,8 +20,6 @@ type chartRowV2Props = {
   context: string;
   year: number;
 };
-
-type Point = { x: number; y: number | null };
 
 export const ChartRowV2 = (props: chartRowV2Props) => {
   const { data, unitNames, context, year } = props;
@@ -40,6 +30,14 @@ export const ChartRowV2 = (props: chartRowV2Props) => {
   const dataFormat = data.format ? data.format : ",.0%";
   const percentage = dataFormat.includes("%");
 
+  const { barData, lineData, uniqueYears } = formatData(
+    data,
+    unitNames,
+    context,
+    year,
+    dataFormat,
+  );
+
   if (data.data === undefined) {
     return <div>No data</div>;
   }
@@ -49,82 +47,6 @@ export const ChartRowV2 = (props: chartRowV2Props) => {
   const handleChange = (event: SelectChangeEvent) => {
     setFigureType(event.target.value as string);
   };
-
-  // Format to {x, y}
-  const reshapedData = unitNames.map((unitName: string) => {
-    return data
-      .data!.filter((row: DataPoint) => {
-        return row.unitName === unitName && row.context === context;
-      })
-      .map((row: DataPoint) => {
-        return { x: row.year, y: row.var } as Point;
-      });
-  });
-
-  // Get the years from the data
-  const years = reshapedData
-    .map((row: Point[]) => {
-      return row.map((point) => point.x);
-    })
-    .flat();
-
-  // Unique years
-  const uniqueYears = [...new Set(years)];
-
-  // missing years from each series
-  const missingYears = reshapedData.map((row: Point[]) => {
-    return uniqueYears.filter(
-      (year: number) => !row.map((point: Point) => point.x).includes(year),
-    );
-  });
-
-  // Make datapoints for the missing years with value null
-  const missingData = missingYears.map((row) => {
-    return row.map((element) => {
-      return { x: element, y: null } as Point;
-    });
-  });
-
-  // Combine and sort by year
-  const paddedData = reshapedData
-    .map((row, i) => {
-      return row.concat(missingData[i]);
-    })
-    .map((row) => {
-      return row.sort((a: Point, b: Point) => {
-        if (a.x < b.x) {
-          return -1;
-        }
-
-        if (a.x > b.x) {
-          return 1;
-        }
-
-        return 0;
-      });
-    });
-
-  // Add unit name label
-  const lineData = paddedData.map((row, i) => {
-    return {
-      data: row.map((point) => point.y),
-      label: unitNames[i],
-      curve: "linear",
-      type: "line",
-      connectNulls: true,
-      valueFormatter: (value: number) => customFormat(dataFormat)(value),
-    } as LineSeriesType;
-  });
-
-  const barData = paddedData
-    .map((row) => {
-      return row
-        .filter((point) => {
-          return point.x === year;
-        })
-        .map((row) => row.y);
-    })
-    .flat();
 
   const barValueFormatter = (value: number | null) => {
     return `${value && customFormat(dataFormat)(value)}`;
@@ -175,39 +97,16 @@ export const ChartRowV2 = (props: chartRowV2Props) => {
           />
         ) : figureType === "bar" ? (
           <Box width={"100%"}>
-            <ChartDataProvider
-              series={[
-                {
-                  type: "bar",
-                  layout: "horizontal",
-                  data: barData,
-                  valueFormatter: barValueFormatter,
-                },
-              ]}
-              height={figureHeight}
-              yAxis={[{ scaleType: "band", data: unitNames, position: "left" }]}
-              xAxis={[
-                {
-                  min: 0,
-                  max: percentage ? 1 : undefined,
-                  position: "bottom",
-                  valueFormatter: valueAxisFormatter,
-                },
-              ]}
-            >
-              <ChartsTooltip />
-              <ChartsSurface>
-                <BarBackground
-                  data={data}
-                  percentage={percentage}
-                  figureHeight={figureHeight}
-                  backgroundMargin={backgroundMargin}
-                />
-                <ChartsXAxis />
-                <ChartsYAxis />
-                <BarPlot />
-              </ChartsSurface>
-            </ChartDataProvider>
+            <MuiBarChart
+              barData={barData}
+              data={data}
+              figureHeight={figureHeight}
+              backgroundMargin={backgroundMargin}
+              unitNames={unitNames}
+              percentage={percentage}
+              barValueFormatter={barValueFormatter}
+              valueAxisFormatter={valueAxisFormatter}
+            />
           </Box>
         ) : null}
       </Box>
