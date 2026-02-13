6aeb39338a74cad9e5014be2ef049f5d4447489c
stein-kato <steinkato.lindberg@yahoo.no>
2026-02-13 15:14:57 +0100

    Knapper

--- apps/skde/pages/behandlingskvalitetV2/index.tsx
+++ apps/skde/pages/behandlingskvalitetV2/index.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import {
-  Box,
   CssBaseline,
   Link,
   ThemeProvider,
   FormControl,
   Select,
   MenuItem,
-  FormHelperText,
   InputLabel,
-  SelectChangeEvent
+  SelectChangeEvent,
+  Stack,
+  Button,
 } from "@mui/material";
 import {
   useRegisterNamesQuery,
@@ -27,9 +27,7 @@ import { Footer } from "../../src/components/Footer";
 import { PageWrapper } from "../../src/components/StyledComponents/PageWrapper";
 import { IndicatorTableSkeleton } from "qmongjs";
 import { LayoutHead } from "../../src/components/LayoutHead";
-import {
-  defaultTableContext,
-} from "../../src/utils/valueOrDefault";
+import { defaultTableContext } from "../../src/utils/valueOrDefault";
 import {
   ColourMap,
   updateColourMap,
@@ -38,8 +36,9 @@ import {
 import checkParamsReady from "../../src/utils/checkParamsReady";
 
 export default function TreatmentQualityPage() {
-
   const [useBeta, setUseBeta] = useState(false);
+
+  const numberOfYearOptions = 5;
 
   const defaultTreatmentUnits = ["Nasjonalt"];
 
@@ -61,7 +60,7 @@ export default function TreatmentQualityPage() {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const unitNamesQuery: UseQueryResult<any, unknown> = useUnitNamesQuery(
-    "all",
+    selectedMedicalFields[0] ? selectedMedicalFields[0] : "all",
     selectedTableContext,
     dataQualitySelected ? "dg" : "ind",
   );
@@ -89,35 +88,10 @@ export default function TreatmentQualityPage() {
     medicalFields: selectedMedicalFields,
   });
 
-  const registers = registryNameQuery?.data;
-  const medicalFields = medicalFieldsQuery?.data;
-  const treatmentUnits = unitNamesQuery?.data && unitNamesQuery?.data.opts_tu[0].options.map(row => row.label)
+  updateColourMap(colourMap, setColourMap, selectedTreatmentUnits);
 
-    updateColourMap(
-      colourMap,
-      setColourMap,
-      selectedTreatmentUnits,
-    );
-
-    const handleMedfieldChange = (event: SelectChangeEvent) => {
-    const {
-      target: { value },
-    } = event;
-    setSelectedMedicalFields(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
-  };
-
-  const handleUnitChange = (event: SelectChangeEvent) => {
-    const {
-      target: { value },
-    } = event;
-    setSelectedTreatmentUnits(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
-    console.log(selectedTreatmentUnits)
+  const handleYearChange = (event: SelectChangeEvent) => {
+    setSelectedYear(Number(event.target.value));
   };
 
   return (
@@ -144,71 +118,51 @@ export default function TreatmentQualityPage() {
           </Link>{" "}
           for mer informasjon.
         </TreatmentQualityAppBar>
-        <Box>
-          <FormControl sx={{ m: 3, minWidth: 200 }}>
-            <InputLabel>Register</InputLabel>
+        <Stack direction="row" padding={2} spacing={1}>
+          <Button variant="outlined">Velg fagområde</Button>
+          <Button variant="outlined">Velg behandlingsenheter</Button>
+          <FormControl>
+            <InputLabel>År</InputLabel>
             <Select
-              value={selectedMedicalFields[0]}
-              label="Register"
-              onChange={handleMedfieldChange}
+              value={selectedYear.toString()}
+              label="År"
+              onChange={handleYearChange}
             >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {registers && registers.map((register) => {
-                return(
-                  <MenuItem value={register.rname}>
-                    {register.short_name}
-                  </MenuItem>
-                )
-              })}
+              {[
+                ...Array(numberOfYearOptions)
+                  .keys()
+                  .map((i: number) => {
+                    const year = defaultYear - i;
+                    return <MenuItem value={year}>{year}</MenuItem>;
+                  }),
+              ]}
             </Select>
           </FormControl>
-          <FormControl sx={{ m: 3, minWidth: 200 }}>
-            <InputLabel>Behandlingshenhet</InputLabel>
-            <Select
-              value={selectedTreatmentUnits[0]}
-              label="Behandlingshenhet"
-              onChange={handleUnitChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {treatmentUnits && treatmentUnits.map((unit) => {
-                return(
-                  <MenuItem value={unit}>
-                    {unit}
-                  </MenuItem>
-                )
-              })}
-            </Select>
-          </FormControl>
-        </Box>
-          {queriesReady && paramsReady ? (
-            <IndicatorTableBodyV2
-              key={"indicator-table2"}
-              context={selectedTableContext}
-              unitNames={getSortedList(
-                colourMap,
-                selectedTreatmentUnits,
-                "units",
-              )}
-              year={selectedYear}
-              type={dataQualitySelected ? "dg" : "ind"}
-              levels={selectedLevel}
-              medfields={selectedMedicalFields}
-              chartColours={getSortedList(
-                colourMap,
-                selectedTreatmentUnits,
-                "colours",
-              )}
-            />
-          ) : (
-            <IndicatorTableSkeleton nRows={10} />
-          )}
+        </Stack>
+        {queriesReady && paramsReady ? (
+          <IndicatorTableBodyV2
+            key={"indicator-table2"}
+            context={selectedTableContext}
+            unitNames={getSortedList(
+              colourMap,
+              selectedTreatmentUnits,
+              "units",
+            )}
+            year={selectedYear}
+            type={dataQualitySelected ? "dg" : "ind"}
+            levels={selectedLevel}
+            medfields={selectedMedicalFields}
+            chartColours={getSortedList(
+              colourMap,
+              selectedTreatmentUnits,
+              "colours",
+            )}
+          />
+        ) : (
+          <IndicatorTableSkeleton nRows={10} />
+        )}
         <Footer page="behandlingskvalitet" />
       </PageWrapper>
-  
     </ThemeProvider>
   );
 }
