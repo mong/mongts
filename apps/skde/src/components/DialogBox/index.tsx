0376558a4c400dc45b26dac9df949c18bb6f2d41
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2026-02-17 14:03:34 +0100

    Fjerne sidemeny i BehandlingskvalitetV2 (#4337)

---
+++ apps/skde/src/components/DialogBox/index.tsx
@@ -0,0 +1,71 @@
+import {
+  Dialog,
+  DialogTitle,
+  Select,
+  MenuItem,
+  DialogContent,
+  DialogActions,
+  Button,
+  SelectChangeEvent,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+import { Dispatch, SetStateAction, useState } from "react";
+import { UseQueryResult } from "@tanstack/react-query";
+import { useMedicalFieldsQuery } from "qmongjs";
+
+type MedicalFieldPopupProps = {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  onSubmit: Dispatch<SetStateAction<string[]>>;
+};
+
+export const MedicalFieldPopup = (props: MedicalFieldPopupProps) => {
+  const { open, setOpen, onSubmit } = props;
+
+  const [selection, setSelection] = useState<string>("");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  const medicalFieldsQuery: UseQueryResult<any, unknown> =
+    useMedicalFieldsQuery();
+
+  const registries = medicalFieldsQuery.data
+    ? medicalFieldsQuery.data.map((row) => row.registers).flat()
+    : [];
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    onSubmit([selection]);
+    setOpen(false);
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const {
+      target: { value },
+    } = event;
+    setSelection(value);
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle>Set backup account</DialogTitle>
+      <DialogContent>
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel>Fagområde</InputLabel>
+          <Select value={selection} onChange={handleChange}>
+            {registries.map((registry) => {
+              return <MenuItem value={registry}>{registry}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleSubmit}>Subscribe</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
