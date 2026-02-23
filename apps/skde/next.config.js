b08a6a0b15234e4c9440e22e58e1dfa66777a7cd
Uglemat <mattias.ugelvik@helse-nord.no>
2026-02-23 13:07:36 +0100

    Tester redirect for de nye sidene til helseatlas rapporter (#4355)
    Når vi går over til ny løsning for helseatlas rapporter må vi sørge for
    at de gamle lenkene på apps.skde.no redirecter dit. Dette er relativt
    lett i og med at sluggene er de samme som før. Jeg tenkte vi kunne teste
    disse redirectene først (/helseatlas-test/) for å se at det funker, sånn
    at vi har det klart til når vi faktisk skal gå over til den nye
    løsningen. Da endrer vi bare /helseatlas-test/ til /helseatlas/, og
    setter permanent: true.

--- apps/skde/next.config.js
+++ apps/skde/next.config.js
@@ -9,4 +9,28 @@ module.exports = {
   experimental: {
     largePageDataBytes: 1024 * 1000,
   },
+  async redirects() {
+    return [
+      {
+        source: "/helseatlas-test/",
+        destination: "https://analyser.skde.no/no/rapporter",
+        permanent: false,
+      },
+      {
+        source: "/helseatlas-test/en/",
+        destination: "https://analyser.skde.no/en/rapporter",
+        permanent: false,
+      },
+      {
+        source: "/helseatlas-test/v2/:slug",
+        destination: "https://analyser.skde.no/no/rapporter/:slug",
+        permanent: false,
+      },
+      {
+        source: "/helseatlas-test/en/v2/:slug",
+        destination: "https://analyser.skde.no/en/rapporter/:slug",
+        permanent: false,
+      },
+    ];
+  },
 };
