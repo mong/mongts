e3cb93020bf464f0a5c06ee3fba13ff80e73fc42
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2026-03-02 13:08:44 +0100

    Opprydning i dialogbokser (#4386)
    Kommentarer, variabelnavn og felles konfig.

---
+++ apps/skde/src/components/DialogBox/styles.tsx
@@ -0,0 +1,17 @@
+// Colour scheme
+export const columnColour1 = "#F7FBFF";
+export const columnColour2 = "#E0F1FF";
+export const columnColour3 = "#F2F9FF";
+
+// The checkbox component has a circular are that is highlighted on mouse hover.
+// This is called a ripple and extends beyond the form control component.
+// For alignment, the checkbox must be moved towards the right by the ripple offset.
+
+// By inspection if the checkbox size is set to medium
+const checkboxWidth = 18;
+const rippleWidth = 42;
+// Calculated offset
+export const rippleOffset = (rippleWidth - checkboxWidth) / 2 - 1;
+
+export const borderRadius = "8px";
+export const marginTop = "8px";
