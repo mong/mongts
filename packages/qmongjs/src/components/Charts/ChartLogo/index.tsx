a1956991edf4069dd22530b7b842fc676d14d12a
stein-kato <steinkato.lindberg@yahoo.no>
2026-03-20 15:25:02 +0100

    Logo-komponenter

---
+++ packages/qmongjs/src/components/Charts/ChartLogo/index.tsx
@@ -0,0 +1,65 @@
+import { IndicatorData } from "types";
+import { useXScale } from "@mui/x-charts";
+
+type LineChartLogoProps = {
+  years: number[];
+  width: number;
+};
+
+export const LineChartLogo = (props: LineChartLogoProps) => {
+  const { years, width } = props;
+
+  const xMax = Math.max(...years);
+
+  const xScale = useXScale();
+
+  const xStop = xScale(xMax);
+
+  const widthHeightRatio = 5 / 2;
+
+  return (
+    <img
+      src="/img/logos/logo-skde-graa.svg"
+      width={width}
+      style={{
+        position: "relative",
+        left: xStop && xStop - width,
+        top: width / widthHeightRatio,
+      }}
+    />
+  );
+};
+
+type BarChartLogoProps = {
+  data: IndicatorData;
+  figureHeight: number;
+  percentage: boolean;
+  backgroundMargin: number;
+  lines: boolean;
+  zoom: boolean;
+  xMaxLimit: number;
+  width: number;
+};
+
+export const BarChartLogo = (props: BarChartLogoProps) => {
+  const { percentage, zoom, xMaxLimit, width } = props;
+
+  const xMax = percentage && !zoom ? 1 : xMaxLimit;
+
+  const xScale = useXScale();
+  const xStop = xScale(xMax);
+
+  const widthHeightRatio = 5 / 2;
+
+  return (
+    <img
+      src="/img/logos/logo-skde-graa.svg"
+      width={width}
+      style={{
+        position: "relative",
+        left: xStop && xStop - width,
+        top: width / widthHeightRatio,
+      }}
+    />
+  );
+};
