02f4d2f7df5eca8f02f45197a811a62e44b4ae62
stein-kato <steinkato.lindberg@yahoo.no>
2026-04-13 11:23:25 +0200

    URL-parameter for behandlingsenheter

--- packages/qmongjs/src/app_config.ts
+++ packages/qmongjs/src/app_config.ts
@@ -59,6 +59,7 @@ export const mainQueryParamsConfig = {
   chart_show_level: withDefault(BooleanParam, undefined),
   chart_show_N: withDefault(BooleanParam, undefined),
   registries: withDefault(DelimitedArrayParam, undefined),
+  units: withDefault(DelimitedArrayParam, undefined),
 };
 
 /** List of hospitals shown on main page of Behandlingskvalitet and Sykehusprofil apps **/
