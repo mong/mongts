4758a2c78cde16fe8d0d1b7da3035688d432f1ed
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2026-05-22 12:07:47 +0200

    fix: revert nye overskrifter i gammel behandlingskvalitet (#4664)

--- apps/skde/pages/behandlingskvalitetV2/index.tsx
+++ apps/skde/pages/behandlingskvalitetV2/index.tsx
@@ -34,7 +34,7 @@ import { MedicalFieldPopup } from "../../src/components/DialogBox/MedicalFieldPo
 import { useQueryParam } from "use-query-params";
 import { mainQueryParamsConfig } from "qmongjs";
 import { PageWrapper } from "../../src/components/StyledComponents/PageWrapper";
-import { IndicatorTable } from "../../src/components/IndicatorTable/Indicatortable";
+import { IndicatorTableV2 } from "../../src/components/IndicatorTable/Indicatortable";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import DoneIcon from "@mui/icons-material/Done";
 
@@ -198,7 +198,7 @@ export default function TreatmentQualityPage() {
               </Button>
             </Stack>
             {selectedMedicalFields.length > 0 ? (
-              <IndicatorTable
+              <IndicatorTableV2
                 key={"indicator-table2"}
                 context={selectedTableContext}
                 unitNames={getSortedList(
