fb5625eeb427e55f89a4f08b96c879e0391dd08e
stein-kato <steinkato.lindberg@yahoo.no>
2026-05-22 12:03:14 +0200

    fix: revert nye overskrifter i gammel behandlingskvalitet

--- apps/skde/src/components/IndicatorTable/Indicatortable/index.tsx
+++ apps/skde/src/components/IndicatorTable/Indicatortable/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { StyledTable } from "./IndicatorTableStyles";
 import { useSearchParams } from "next/navigation";
 import { RegistrySection } from "./RegistrySection";
+import { RegistrySectionV2 } from "./RegistrySectionV2";
 
 type IndicatorTableProps = {
   context: string;
@@ -29,6 +30,38 @@ export const IndicatorTable = (props: IndicatorTableProps) => {
     <StyledTable sx={{ marginTop: "0.625rem" }}>
       {medfields.map((medfield) => (
         <RegistrySection
+          key={medfield}
+          levels={levels}
+          unitNames={unitNames}
+          medfield={medfield}
+          openRowID={openRowID}
+          setOpenRowID={setOpenRowID}
+          context={context}
+          type={type}
+          year={year}
+          chartColours={chartColours}
+        />
+      ))}
+    </StyledTable>
+  );
+};
+
+export const IndicatorTableV2 = (props: IndicatorTableProps) => {
+  const { context, type, year, unitNames, levels, medfields, chartColours } =
+    props;
+
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams.toString());
+  const openRowParam = params.get("selected_row");
+
+  const [openRowID, setOpenRowID] = useState<string>(
+    openRowParam ? openRowParam : "",
+  );
+
+  return (
+    <StyledTable sx={{ marginTop: "0.625rem" }}>
+      {medfields.map((medfield) => (
+        <RegistrySectionV2
           key={medfield}
           levels={levels}
           unitNames={unitNames}
