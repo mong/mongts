6813110ec6ef05052068bfc392796916d401b407
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2026-05-22 16:02:15 +0200

    feat: sticky toolbar i BKV2 (#4677)

---
+++ apps/skde/src/components/IndicatorTable/Indicatortable/StickyHeader.tsx
@@ -0,0 +1,36 @@
+import { AppBar, Toolbar } from "@mui/material";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
+
+type Props = {
+  children?: React.ReactElement<unknown>;
+};
+
+const HideOnScroll = (props: Props) => {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    target: undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children ?? <div />}
+    </Slide>
+  );
+};
+
+type AppBarProps = {
+  children?: React.ReactElement<unknown>;
+};
+
+export const TreatmentQualityAppBarV2 = ({ children }: AppBarProps) => {
+  return (
+    <>
+      <HideOnScroll {...children}>
+        <AppBar position="sticky" color="secondary">
+          <Toolbar>{children}</Toolbar>
+        </AppBar>
+      </HideOnScroll>
+    </>
+  );
+};
