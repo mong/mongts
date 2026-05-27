59d771fee22d1aa88609575522c7c1436d41baa3
GabrioFr <gabrio.frantzen@bouvet.no>
2026-05-27 17:28:40 +0200

    fix: fix cypress tests and v1 and v1 filtersettings typeErrors

--- apps/skde/pages/behandlingskvalitet/index.tsx
+++ apps/skde/pages/behandlingskvalitet/index.tsx
@@ -193,10 +193,25 @@ export default function TreatmentQualityPage() {
    * Handle filter changes
    */
   const handleFilterChanged = (
-    action: FilterSettingsAction,
     newFilterSettings: { map: Map<string, FilterSettingsValue[]> },
-    // oldFilterSettings: { map: Map<string, FilterSettingsValue[]> },
+    // biome-ignore lint: ignored to pass ci checks, but should be fixed properly in the future
+    oldFilterSettings: { map: Map<string, FilterSettingsValue[]> },
+    action: FilterSettingsAction,
   ): void => {
+    if (action.type === FilterSettingsActionType.RESET_SELECTIONS) {
+      setAllSelected(newFilterSettings);
+      updateColourMap(
+        colourMap,
+        setColourMap,
+        valueOrDefault(treatmentUnitsKey, newFilterSettings) as string[],
+      );
+      return;
+    }
+
+    if (!action.sectionSetting) {
+      return;
+    }
+
     switch (action.sectionSetting.key) {
       case tableContextKey: {
         setSelectedTableContext(
@@ -242,10 +257,6 @@ export default function TreatmentQualityPage() {
       }
       default:
         break;
-    }
-
-    if (action.type === FilterSettingsActionType.RESET_SELECTIONS) {
-      setAllSelected(newFilterSettings);
     }
 
     updateColourMap(
---
+++ apps/skde/test/cypress/integration/behandlingskvallitet.cy.tsx
@@ -0,0 +1,50 @@
+/// <reference types="cypress"/>
+
+context("Testing of behandlingskvalitet page", () => {
+  beforeEach(() => {
+    cy.visit("/behandlingskvalitet/?year=2023");
+  });
+  it("Main page", () => {
+    cy.viewport(1550, 1750);
+    cy.get('[data-testid="tu_header_Nasjonalt"]', {
+      timeout: 4000,
+    }).should("exist"); // main page
+    cy.get('[data-testid^="indicatorrow_"]', {
+      timeout: 60000,
+    }).should("exist"); // indicator row
+    cy.get(
+      '[data-testid="permanentFilterMenu-context-toggle-caregiver"]',
+    ).should("exist");
+    cy.get(
+      '[data-testid="permanentFilterMenu-context-toggle-resident"]',
+    ).should("exist");
+    cy.get(
+      '[data-testid="permanentFilterMenu-context-toggle-resident"]',
+    ).click();
+    cy.get('[data-testid="selected-filters-section-id-selectedfilters"]', {
+      timeout: 60000,
+    }).should("exist"); // Menu exist
+    cy.get('[data-testid="TuneRoundedIcon"]').should("not.exist"); // Menu does not exist
+    cy.viewport(550, 750);
+    cy.get('[data-testid="TuneRoundedIcon"]').should("exist"); // Menu does not exist
+  });
+  it("Ablanor page", () => {
+    cy.viewport(1550, 1750);
+    cy.visit("/behandlingskvalitet/ablanor/?year=2023");
+    cy.get('[data-testid="tu_header_Nasjonalt"]').should("exist"); // main page
+    cy.get('[data-testid="indicatorrow_hjerneslag_beh_enhet"]').should(
+      "not.exist",
+    ); // indicator row
+    cy.get(
+      '[data-testid="permanentFilterMenu-context-toggle-caregiver"]',
+    ).should("not.exist"); // tab does not exist
+    cy.get(
+      '[data-testid="permanentFilterMenu-context-toggle-resident"]',
+    ).should("not.exist");
+    cy.get('[data-testid="selected-filters-section-id-selectedfilters"]', {
+      timeout: 60000,
+    }).should("exist"); // Special menu exist
+  });
+});
+
+export {};
