82a7376778aa585912f1bef7ba9e3fc675617b83
stein-kato <steinkato.lindberg@yahoo.no>
2026-08-12 12:34:11 +0200

    chore: fjerne flere ubrukte ting

--- apps/skde/src/app_config.ts
+++ apps/skde/src/app_config.ts
@@ -20,27 +20,6 @@ interface appTextTypes {
   };
   tu_list: { header_text: string; max_nr_tu: number };
 }
-
-export const app_text: appTextTypes = {
-  menus: {
-    unit: "Velg behandlingssted",
-    year: "År:",
-  },
-  indicators: {
-    high: { text: "Høy måloppnåelse" },
-    moderate: { text: "Moderat måloppnåelse" },
-    low: { text: "Lav måloppnåelse" },
-  },
-  table: {
-    main_column: "Kvalitetsindikator",
-    national_column: "Nasjonalt",
-    desired_level: "Ønsket målnivå",
-  },
-  tu_list: {
-    header_text: "Velg behandlingssted(er)",
-    max_nr_tu: 5,
-  },
-};
 export const maxYear = 2025;
 export const minYear = maxYear - 4;
 export const defaultYear = 2025;
