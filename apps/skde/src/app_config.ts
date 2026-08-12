ef40cb8181d0e9d222b9b3983636b17559065181
stein-kato <steinkato.lindberg@yahoo.no>
2026-08-12 12:46:16 +0200

    chore: slett mer

--- apps/skde/src/app_config.ts
+++ apps/skde/src/app_config.ts
@@ -6,20 +6,6 @@ import {
   withDefault,
 } from "use-query-params";
 
-interface appTextTypes {
-  menus: { unit: string; year: string };
-  indicators: {
-    high: { text: string };
-    moderate: { text: string };
-    low: { text: string };
-  };
-  table: {
-    main_column: string;
-    national_column: string;
-    desired_level: string;
-  };
-  tu_list: { header_text: string; max_nr_tu: number };
-}
 export const maxYear = 2025;
 export const minYear = maxYear - 4;
 export const defaultYear = 2025;
