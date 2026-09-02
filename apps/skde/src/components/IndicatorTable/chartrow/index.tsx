3118e61411bf9626d8811a0e53a363c7a506dbba
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2026-09-02 08:38:28 +0200

    fix: filtrer vekk lav DG og lav N i linjediagram (#4955)

--- apps/skde/src/components/IndicatorTable/chartrow/index.tsx
+++ apps/skde/src/components/IndicatorTable/chartrow/index.tsx
@@ -70,6 +70,18 @@ export const ChartRow = (props: ChartRowProps) => {
     return <div>No data</div>;
   }
 
+  // Filterer vekk lav dekningsgrad
+  data.data = data.data.filter((row) =>
+    row.dg !== null ? row.dg >= 0.6 : true,
+  );
+
+  // Filterer vekk lav n
+  data.data = data.data.filter((row) =>
+    data.minDenominator
+      ? row.denominator >= data.minDenominator
+      : row.denominator >= 5,
+  );
+
   // Callback functions for dropdown menus
   const handleBarChartTypeChange = (event: SelectChangeEvent) => {
     setBarChartType(event.target.value as string);
