e8afdc39c5807801691d20d7861d352458370713
Stein-Kato <64078729+Stein-Kato@users.noreply.github.com>
2026-09-14 14:41:57 +0200

    fix(BehandlingskvalitetV3): popup for datakvalitet fungerer (#4997)

--- apps/skde/src/components/IndicatorTable/IndicatortableV3/index.tsx
+++ apps/skde/src/components/IndicatorTable/IndicatortableV3/index.tsx
@@ -88,7 +88,7 @@ const reshapeData = (
               <ChartRowV2
                 data={indicator}
                 unitNames={unitNames}
-                medfield={registry.registerShortName}
+                medfield={registry.registerName}
                 context={context}
                 year={year}
                 treatmentUnitsByLevel={unitNamesByLevel}
