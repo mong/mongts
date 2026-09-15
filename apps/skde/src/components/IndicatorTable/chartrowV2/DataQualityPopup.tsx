1852cbab85bd1db6c80e31b474eb5bac8f6f00a7
stein-kato <steinkato.lindberg@yahoo.no>
2026-09-15 13:58:09 +0200

    fix: ikke vis datakvalitetsknapp i datakvalitetspopup

--- apps/skde/src/components/IndicatorTable/chartrowV2/DataQualityPopup.tsx
+++ apps/skde/src/components/IndicatorTable/chartrowV2/DataQualityPopup.tsx
@@ -132,7 +132,7 @@ export const DataQualityPopup = (props: CoveragePopupProps) => {
           treatmentUnitsByLevel={treatmentUnitsByLevel}
           indID={dataQualityIndId}
           registryName={registryName}
-          showDGButton={true}
+          showDGButton={false}
         />
       </DialogContent>
       <Box>
