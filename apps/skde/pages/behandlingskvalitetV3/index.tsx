24b566cb1eb83e4fe48ecee1d2b477ddff87f8d6
stein-kato <steinkato.lindberg@yahoo.no>
2026-09-18 15:04:57 +0200

    feat: ny hovedversjon av Behandlingskvalitet (pizza)

--- apps/skde/pages/behandlingskvalitetV3/index.tsx
+++
@@ -1,5 +0,0 @@
-import { TreatmentQualityPageV3 } from "../../src/components/IndicatorTable/TreatmentQualityPageV3";
-
-export default function TreatmentQualityPage() {
-  return <TreatmentQualityPageV3 />;
-}
