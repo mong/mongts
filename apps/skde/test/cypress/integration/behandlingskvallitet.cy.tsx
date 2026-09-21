7abb837ad40e591ab64112e1f8f6e323b282a2a5
stein-kato <steinkato.lindberg@yahoo.no>
2026-09-21 12:38:00 +0200

    fix: sett timeout på Cypress

--- apps/skde/test/cypress/integration/behandlingskvallitet.cy.tsx
+++ apps/skde/test/cypress/integration/behandlingskvallitet.cy.tsx
@@ -2,7 +2,7 @@
 
 context("Testing of behandlingskvalitet page", () => {
   beforeEach(() => {
-    cy.visit("behandlingskvalitet/?registries=hjerneslag");
+    cy.visit("behandlingskvalitet/?registries=hjerneslag", { timeout: 10000});
   });
   it("Main page", () => {
     
