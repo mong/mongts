3b77c74b6f2e2216c939e269e53f2ef445d2e876
GabrioFr <gabrio.frantzen@bouvet.no>
2026-09-22 10:38:54 +0200

    build(treatment-quality): app-router optimizations

--- apps/skde/app/behandlingskvalitetV3/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { TreatmentQualityPageV3 } from "../../src/components/IndicatorTable/TreatmentQualityPageV3";
-
-export default function TreatmentQualityPage() {
-  return <TreatmentQualityPageV3 />;
-}
