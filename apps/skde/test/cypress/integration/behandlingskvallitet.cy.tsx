4ac911d4ebb8c55564763d3470e20180604d5bb0
GabrioFr <gabrio.frantzen@bouvet.no>
2026-09-25 11:16:43 +0200

    chore(cypress): add delay for api-calls to cypress tests

--- apps/skde/test/cypress/integration/behandlingskvallitet.cy.tsx
+++ apps/skde/test/cypress/integration/behandlingskvallitet.cy.tsx
@@ -2,23 +2,24 @@
 
 context("Testing of behandlingskvalitet page", () => {
   beforeEach(() => {
-    cy.visit("behandlingskvalitet/?registries=hjerneslag", { timeout: 20000});
+    cy.visit("behandlingskvalitet/?registries=hjerneslag", { timeout: 20000 });
   });
   it("Main page", () => {
-    
-    cy.get('[data-testid="IndicatorTable"]').should("exist")
-    
+    cy.get('[data-testid="IndicatorTable"]', { timeout: 20000 }).should(
+      "exist",
+    );
+
     // Test that the medical field pop up works
     cy.get('[data-testid="MedicalFieldPopUpButton"]').should("exist");
     cy.get('[data-testid="MedicalFieldPopUpButton"]').click();
     cy.get('[data-testid="MedicalFieldPopUp"]').should("exist");
-    cy.get('[data-testid="MedicalFieldPopUpSubmit"').click()
+    cy.get('[data-testid="MedicalFieldPopUpSubmit"').click();
 
     // Test that the treatment unit popup works
     cy.get('[data-testid="TreatmentUnitPopUpButton"]').should("exist");
     cy.get('[data-testid="TreatmentUnitPopUpButton"]').click();
     cy.get('[data-testid="TreatmentUnitPopUp"]').should("exist");
-    cy.get('[data-testid="TreatmentUnitPopUpSubmit"').click()
+    cy.get('[data-testid="TreatmentUnitPopUpSubmit"').click();
   });
 });
 
